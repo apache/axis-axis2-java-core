76accba38ed22dc8a6432c46272ee81dba5549f1
Deepal Jayasinghe <deepal@apache.org>
2004-11-22 11:45:20 +0000

    modified the default constructor , since it faild the test case

--- dev/scratch/prototype1/src/test/org/apache/axis/engine/registry/MockFlow.java
+++ dev/scratch/prototype1/src/test/org/apache/axis/engine/registry/MockFlow.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2001-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,11 +17,11 @@
 package org.apache.axis.engine.registry;
 
 
-public class MockFlow extends ConcreateFlow implements Flow{
-    public MockFlow(String message,int length){
+public class MockFlow extends ConcreateFlow implements Flow {
+    public MockFlow(String message, int length) {
         super();
-        for(int i = 0;i<length;i++){
-            SpeakingHandler h1 = new SpeakingHandler("Executing "+ i +" inside "+message);
+        for (int i = 0; i < length; i++) {
+            SpeakingHandler h1 = new SpeakingHandler();//"Executing " + i + " inside " + message);
             super.addHandler(h1);
         }
     }
--- dev/scratch/prototype1/src/test/org/apache/axis/engine/registry/SpeakingHandler.java
+++ dev/scratch/prototype1/src/test/org/apache/axis/engine/registry/SpeakingHandler.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2001-2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,24 +25,29 @@ import org.apache.axis.engine.context.MessageContext;
 public class SpeakingHandler extends AbstractEngineElement implements Handler {
     private String message;
     private QName name;
-    
-    public SpeakingHandler(QName name){
+   /*
+    public SpeakingHandler(QName name) {
         this.name = name;
     }
-    public SpeakingHandler(String message){
+  */
+    public SpeakingHandler() {
+       this.message = "Hi I amtesting ";
+    }
+   /*
+    public SpeakingHandler(String message) {
         this.message = message;
     }
-
+   */
     public QName getName() {
         return name;
     }
 
     public void invoke(MessageContext msgContext) throws AxisFault {
-        System.out.println("I am "+ message +" Handler Running :)");
+        System.out.println("I am " + message + " Handler Running :)");
     }
 
     public void revoke(MessageContext msgContext) {
-        System.out.println("I am "+ message +" Handler Running :)");
+        System.out.println("I am " + message + " Handler Running :)");
     }
 
     public void setName(QName name) {
