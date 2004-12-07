0d0e9d1c0b75d6b751dfbd2113c50c5b32e868e3
Eran Chinthaka <chinthaka@apache.org>
2004-12-07 05:11:09 +0000

    The cleaned up interface with Incomplete WSDL 1.1 To WOM Builder

--- dev/scratch/wsdl_pt_chatura/src/java/org/apache/wsdl/wom/WDSLProperty.java
+++ dev/scratch/wsdl_pt_chatura/src/java/org/apache/wsdl/wom/WDSLProperty.java
@@ -34,39 +34,3 @@ public interface WDSLProperty {
 
     public void setValue(Object value);
 }
-/*
- * Copyright 2001-2004 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.apache.wsdl.wom;
-
-import java.net.URI;
-
-/**
- * @author chathura@opensource.lk
- *
- */
-public interface WDSLProperty {
-    public Object getConstraint();
-
-    public void setConstraint(Object constraint);
-
-    public URI getName();
-
-    public void setName(URI name);
-
-    public Object getValue();
-
-    public void setValue(Object value);
-}
--- dev/scratch/wsdl_pt_chatura/src/java/org/apache/wsdl/wom/WSDLFault.java
+++ dev/scratch/wsdl_pt_chatura/src/java/org/apache/wsdl/wom/WSDLFault.java
@@ -30,35 +30,3 @@ public interface WSDLFault {
 
 	public void setName(String name);
 }
-/*
- * Copyright 2001-2004 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.apache.wsdl.wom;
-
-import org.w3c.dom.Element;
-
-/**
- * @author chathura@opensource.lk
- *
- */
-public interface WSDLFault {
-	public Element getElement();
-
-	public void setElement(Element element);
-
-	public String getName();
-
-	public void setName(String name);
-}
