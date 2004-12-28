d14132d6385eac608cd5f59e09f686267726410a
Ajith Ranabahu <ajith@apache.org>
2004-12-28 06:30:24 +0000

    Imporved the comparison test case so that it compares the attributes as well

--- dev/scratch/prototype2/src/test/org/apache/axis/om/OMTestUtils.java
+++ dev/scratch/prototype2/src/test/org/apache/axis/om/OMTestUtils.java
@@ -1,26 +1,24 @@
 /*
- * Copyright 2001-2004 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2001-2004 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package org.apache.axis.om;
 
 import junit.framework.TestCase;
 import org.apache.axis.impl.llom.factory.OMXMLBuilderFactory;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
@@ -35,26 +33,26 @@ public class OMTestUtils {
         OMXMLParserWrapper builder =   OMXMLBuilderFactory.createStAXSOAPModelBuilder(OMFactory.newInstance(),parser);
         return builder;
     }
-    
+
     public static void walkThrough(OMElement omEle){
         Iterator attibIt = omEle.getAttributes();
         while(attibIt.hasNext()){
             TestCase.assertNotNull("once the has next is not null, the " +
-                            "element should not be null",attibIt.next());
+                    "element should not be null",attibIt.next());
         }
         Iterator it = omEle.getChildren();
         while(it.hasNext()){
             OMNode ele = (OMNode)it.next();
             TestCase.assertNotNull("once the has next is not null, the " +
-                "element should not be null",ele);
-            
+                    "element should not be null",ele);
+
             if(ele instanceof OMElement){
                 walkThrough((OMElement)ele);
             }    
         }
-    
+
     }
-    
+
     public static void compare(Element ele,OMElement omele) throws Exception{
         if(ele == null && omele == null){
             return;
@@ -62,9 +60,26 @@ public class OMTestUtils {
 
             TestCase.assertTrue(ele.getLocalName().equals(omele.getLocalName()));
             TestCase.assertTrue(ele.getNamespaceURI().equals(omele.getNamespace().getName()));
-            
+
+            //go through the attributes
+            NamedNodeMap map =  ele.getAttributes();
+            Iterator attIterator = omele.getAttributes();
+            OMAttribute OMattribute;
+            Attr DOMAttribute;
+            String DOMAttrName;
+            int length = map.getLength();
+            for (int i=0;i<length;i++){
+                DOMAttribute = (Attr)map.item(i);
+                DOMAttrName = DOMAttribute.getName();
+                if (!DOMAttrName.startsWith("xmlns")){
+                    TestCase.assertTrue(attIterator.hasNext());
+                    OMattribute = (OMAttribute)attIterator.next();
+                    TestCase.assertEquals(DOMAttribute.getLocalName(),OMattribute.getLocalName());
+                }
+            }
+
+
             Iterator it = omele.getChildren();
-            
             NodeList list = ele.getChildNodes();
             for(int i = 0;i<list.getLength();i++){
                 Node node = list.item(i);
@@ -78,11 +93,13 @@ public class OMTestUtils {
                     compare((Element)node,(OMElement)tempOmNode);
                 }
             }
+
+
         }else{
-               throw new Exception("One is null");
+            throw new Exception("One is null");
         }
     }
-    
+
     public static SOAPEnvelope createOM(File file) throws Exception{
         return (SOAPEnvelope)getOMBuilder(file).getDocumentElement();
     }
