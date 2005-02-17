e7741b515337e547a86d5b5d8f10f3e73987549a
Ajith Ranabahu <ajith@apache.org>
2005-02-17 12:23:49 +0000

    Adding the utility package

---
+++ dev/scratch/prototype2/src/java/org/apache/axis/util/OperationItemBean.java
@@ -0,0 +1,19 @@
+package org.apache.axis.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ajith
+ * Date: Feb 17, 2005
+ * Time: 6:03:36 PM
+ */
+public class OperationItemBean {
+    private String operationName;
+
+    public String getOperationName() {
+        return operationName;
+    }
+
+    public void setOperationName(String operationName) {
+        this.operationName = operationName;
+    }
+}
---
+++ dev/scratch/prototype2/src/java/org/apache/axis/util/ServiceListBean.java
@@ -0,0 +1,23 @@
+package org.apache.axis.util;
+
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ajith
+ * Date: Feb 17, 2005
+ * Time: 5:57:47 PM
+ */
+public class ServiceListBean {
+    private ArrayList serviceList;
+
+    public ServiceListBean() {
+        this.serviceList = new ArrayList();
+    }
+
+    public void addService(ServiceItemBean service){
+        this.serviceList.add(serviceList);
+    }
+
+}
+
