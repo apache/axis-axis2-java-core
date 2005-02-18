e8948f02912cdef9fb55288a49c9aed933606872
Hemapani Srinath Perera <hemapani@apache.org>
2005-02-18 12:21:17 +0000

    fixing test cases

---
+++ modules/samples/src/test/org/apache/axis/integration/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Feb 18, 2005
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package org.apache.axis.integration;
+
+/**
+ * @author hemapani
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Constants {
+
+    public static final int TESTING_PORT = 5555;
+}
