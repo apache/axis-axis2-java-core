9a686eb3881504170352acbb2bb0519e72c65c54
Hemapani Srinath Perera <hemapani@apache.org>
2005-02-18 03:39:10 +0000

    moving the scratch to top level archive dir

--- dev/scratch/gdaniels/EngineToy/src/axis2/Constants.java
+++
@@ -1,9 +0,0 @@
-package axis2;
-
-/**
- * Constants
- */
-public interface Constants {
-    static final String GLOBAL_RECEIVE = "global.receive";
-    static final String GLOBAL_SEND = "global.send";
-}
--- dev/scratch/prototype2/src/samples/userguide/sample1/src/EchoXML.java
+++
@@ -1,7 +0,0 @@
-import org.apache.axis.om.OMElement;
-
-public class EchoXML {
-    public OMElement echo(OMElement element){
-        return element;
-    }
-}
