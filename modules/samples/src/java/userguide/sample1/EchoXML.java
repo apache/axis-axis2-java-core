cdf92b5e7d4168d423c4e40344bb9f9ceb8e17df
Chathura Herath <chathura@apache.org>
2005-03-24 08:53:31 +0000

    Edited the Web service to make sure that the OMElement returned is in properform. i.e. The element is fully built and detached before being returned.

--- modules/samples/src/java/userguide/sample1/EchoXML.java
+++ modules/samples/src/java/userguide/sample1/EchoXML.java
@@ -5,6 +5,8 @@ import org.apache.axis.om.OMElement;
 
 public class EchoXML {
     public OMElement echo(OMElement element){
+        element.getNextSibling();
+        element.detach();
         return element;
     }
 }
