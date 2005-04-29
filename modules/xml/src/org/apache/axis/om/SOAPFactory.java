d7d688bb07502161c6dc5f6173f3e15e60edf1a1
Eran Chinthaka <chinthaka@apache.org>
2005-04-29 12:04:02 +0000

    Adding SOAP and OM Factories. Refer mail to the mailing list for more details

---
+++ modules/xml/src/org/apache/axis/om/SOAPFactory.java
@@ -0,0 +1,107 @@
+package org.apache.axis.om;
+
+import org.apache.axis.om.*;
+
+/**
+ * Copyright 2001-2004 The Apache Software Foundation.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * <p/>
+ */
+public interface SOAPFactory extends OMFactory{
+    /**
+     * Eran Chinthaka (chinthaka@apache.org)
+     */
+    /**
+     * @param envelope
+     * @return
+     */
+    public abstract SOAPBody createSOAPBody(SOAPEnvelope envelope);
+
+    /**
+     * @param envelope
+     * @param builder
+     * @return
+     */
+    public abstract SOAPBody createSOAPBody(SOAPEnvelope envelope,
+                                            OMXMLParserWrapper builder);
+
+    /**
+     * @param ns
+     * @param builder
+     * @return
+     */
+    public abstract SOAPEnvelope createSOAPEnvelope(OMNamespace ns,
+                                                    OMXMLParserWrapper builder);
+
+    /**
+     * @param ns
+     * @return
+     */
+    public abstract SOAPEnvelope createSOAPEnvelope(OMNamespace ns);
+
+    /**
+     * @param envelope
+     * @return
+     */
+    public abstract SOAPHeader createSOAPHeader(SOAPEnvelope envelope);
+
+    /**
+     * @param envelope
+     * @param builder
+     * @return
+     */
+    public abstract SOAPHeader createSOAPHeader(SOAPEnvelope envelope,
+                                                OMXMLParserWrapper builder);
+
+    /**
+     * @param localName
+     * @param ns
+     * @return
+     */
+    public abstract SOAPHeaderBlock createSOAPHeaderBlock(String localName,
+                                                          OMNamespace ns);
+
+    /**
+     * @param localName
+     * @param ns
+     * @param parent
+     * @param builder
+     * @return
+     */
+    public abstract SOAPHeaderBlock createSOAPHeaderBlock(String localName,
+                                                          OMNamespace ns, OMElement parent, OMXMLParserWrapper builder);
+
+    /**
+     * @param parent
+     * @param e
+     * @return
+     */
+    public abstract SOAPFault createSOAPFault(SOAPBody parent, Exception e);
+
+    /**
+     * @param ns
+     * @param parent
+     * @param builder
+     * @return
+     */
+    public abstract SOAPFault createSOAPFault(OMNamespace ns, SOAPBody parent,
+                                              OMXMLParserWrapper builder);
+
+    /**
+     * Method getDefaultEnvelope
+     *
+     * @return
+     */
+    public abstract SOAPEnvelope getDefaultEnvelope();
+}
