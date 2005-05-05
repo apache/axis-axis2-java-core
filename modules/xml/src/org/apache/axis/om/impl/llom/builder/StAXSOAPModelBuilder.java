5f1206c53b1efd04649c826d3d211cee1a15b57e
Eran Chinthaka <chinthaka@apache.org>
2005-05-05 07:50:00 +0000

    Fixing OM Test Failure

--- modules/xml/src/org/apache/axis/om/impl/llom/builder/StAXSOAPModelBuilder.java
+++ modules/xml/src/org/apache/axis/om/impl/llom/builder/StAXSOAPModelBuilder.java
@@ -68,6 +68,7 @@ public class StAXSOAPModelBuilder extends StAXBuilder {
      */
     public StAXSOAPModelBuilder(XMLStreamReader parser) {
         super(parser);
+        soapFactory = OMAbstractFactory.getSOAP11Factory();
         identifySOAPVersion();
 
         parseHeaders();
