73995c3499cebc728959f2cf25eed5b1a347ee53
Sanjiva Weerawarana <sanjiva@apache.org>
2005-05-05 07:16:17 +0000

    Cleaned up ServiceContext and editing OperationContext

--- modules/core/src/org/apache/axis/context/OperationContext.java
+++ modules/core/src/org/apache/axis/context/OperationContext.java
@@ -26,125 +26,132 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author chathura@opensource.lk
- *
- */
-
-/**
- * This class will provide the functionality to support the two basic MEPs
- * IN_OUT and IN_ONLY.
- *
- * @author chathura@opensource.lk
+ * An OperationContext represents a running "instance" of an operation. This
+ * particular implementation of OperationContext supports MEPs which have one
+ * input message and/or one output message. That is, it supports the all the
+ * MEPs that are in the WSDL 2.0 specification.
  */
 
 public class OperationContext extends AbstractContext {
 
-    private ArrayList messageContextList;
+	private ArrayList messageContextList;
 
-    private String MepId;
+	private String MepId;
 
-    private AxisOperation axisOperation;
+	private AxisOperation axisOperation;
 
-    public OperationContext(AxisOperation axisOperation,ServiceContext serviceContext) {
-        super(serviceContext);
-        this.axisOperation = axisOperation;
+	public OperationContext(AxisOperation axisOperation,
+			ServiceContext serviceContext) {
+		super(serviceContext);
+		this.axisOperation = axisOperation;
 
-// Most frequently used MEPs are IN ONLY and IN-OUT MEP. So the number of messagecontext for those MEPs are at most 2. Setting the initial capacity of the arrayList to 2.
-        messageContextList = new ArrayList(2);
-    }
+		// Most frequently used MEPs are IN ONLY and IN-OUT MEP. So the number
+		// of messagecontext for those MEPs are at most 2. Setting the initial
+		// capacity of the arrayList to 2.
+		messageContextList = new ArrayList(2);
+	}
+
 	/**
-	 *
+	 * 
 	 * When a new message is added to the <code>MEPContext</code> the logic
 	 * should be included remove the MEPContext from the table in the
 	 * <code>EngineContext</code>. Example: IN_IN_OUT At the second IN
 	 * message the MEPContext should be removed from the AxisOperation
-	 *
+	 * 
 	 * @param msgContext
 	 */
 	public void addMessageContext(MessageContext msgContext) throws AxisFault {
-		if (WSDLConstants.MEP_URI_IN_ONLY.equals(this.axisOperation.getMessageExchangePattern())){
+		if (WSDLConstants.MEP_URI_IN_ONLY.equals(this.axisOperation
+				.getMessageExchangePattern())) {
 			messageContextList.add(msgContext);
-		} else if(WSDLConstants.MEP_URI_IN_OUT.equals(this.axisOperation.getMessageExchangePattern())){
+		} else if (WSDLConstants.MEP_URI_IN_OUT.equals(this.axisOperation
+				.getMessageExchangePattern())) {
 			messageContextList.add(msgContext);
 		}
 
-		if(this.isComplete())
+		if (this.isComplete())
 			msgContext.getEngineContext().removeMEP(this);
 
+	}
 
-    }
+	/**
+	 * @param index
+	 * @return
+	 */
+	public MessageContext getMessageContext(int index) {
+		return (MessageContext) messageContextList.get(index);
+	}
 
-    /**
-     * @param index
-     * @return
-     */
-    public MessageContext getMessageContext(int index) {
-        return (MessageContext) messageContextList.get(index);
-    }
+	public MessageContext removeMessageContext(MessageContext ctxt) {
+		messageContextList.remove(ctxt.getMessageID());
+		return ctxt;
+	}
 
-    public MessageContext removeMessageContext(MessageContext ctxt) {
-        messageContextList.remove(ctxt.getMessageID());
-        return ctxt;
-    }
+	public List getAllMessageContexts() {
+		return this.messageContextList;
+	}
 
-    public List getAllMessageContexts() {
-        return this.messageContextList;
-    }
+	public MessageContext getMessageContext(String messageID) throws AxisFault {
+		if (null != messageID) {
+			for (int i = 0; i < this.messageContextList.size(); i++) {
+				if (messageID.equals(((MessageContext) (this.messageContextList
+						.get(i))).getMessageID())) {
+					return ((MessageContext) (this.messageContextList.get(i)));
+				}
+			}
+		}
 
-    public MessageContext getMessageContext(String messageID) throws AxisFault {
-        if (null != messageID) {
-            for (int i = 0; i < this.messageContextList.size(); i++) {
-                if (messageID.equals(((MessageContext) (this.messageContextList
-                        .get(i))).getMessageID())) {
-                    return ((MessageContext) (this.messageContextList.get(i)));
-                }
-            }
-        }
+		throw new AxisFault(
+				" Message does not exist in the current MEP : Invalid MessageID :"
+						+ messageID);
+	}
 
-        throw new AxisFault(" Message does not exist in the current MEP : Invalid MessageID :" + messageID);
-    }
+	public void addMessageContext(String messageLabel, MessageContext msgContext)
+			throws AxisFault {
+		// TODO : Chathura
+		throw new UnsupportedOperationException();
 
-    public void addMessageContext(String messageLabel, MessageContext msgContext) throws AxisFault {
-        // TODO : Chathura
-        throw new UnsupportedOperationException();
+	}
 
-    }
+	/**
+	 * @return Returns the mepId.
+	 */
+	public String getMepId() {
+		return MepId;
+	}
 
-    /**
-     * @return Returns the mepId.
-     */
-    public String getMepId() {
-        return MepId;
-    }
+	/**
+	 * @param mepId
+	 *            The mepId to set.
+	 */
+	public void setMepId(String mepId) {
+		MepId = mepId;
+	}
 
-    /**
-     * @param mepId The mepId to set.
-     */
-    public void setMepId(String mepId) {
-        MepId = mepId;
-    }
+	/**
+	 * Chathura, please implement this method to return the last in message of
+	 * the MEP. I want this for addressing - Chinthaka
+	 * 
+	 * @return
+	 */
+	public MessageContext getLastInMessageContext() {
+		throw new UnsupportedOperationException();
 
-    /**
-     * Chathura, please implement this method to return the last in message of the MEP.
-     * I want this for addressing - Chinthaka
-     * @return
-     */
-    public MessageContext getLastInMessageContext(){
-        throw new UnsupportedOperationException();
+	}
 
-    }
+	public void cleanup() throws AxisFault {
+		//TODO Chathura
+		throw new UnsupportedOperationException();
+	}
 
-    public void cleanup() throws AxisFault {
-        //TODO Chathura
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean isComplete(){
-		if (WSDLConstants.MEP_URI_IN_ONLY.equals(this.axisOperation.getMessageExchangePattern())){
-			if(1 == this.messageContextList.size())
+	public boolean isComplete() {
+		if (WSDLConstants.MEP_URI_IN_ONLY.equals(this.axisOperation
+				.getMessageExchangePattern())) {
+			if (1 == this.messageContextList.size())
 				return true;
-		}else if(WSDLConstants.MEP_URI_IN_OUT.equals(this.axisOperation.getMessageExchangePattern())){
-			if(2 == this.messageContextList.size())
+		} else if (WSDLConstants.MEP_URI_IN_OUT.equals(this.axisOperation
+				.getMessageExchangePattern())) {
+			if (2 == this.messageContextList.size())
 				return true;
 		}
 
--- modules/core/src/org/apache/axis/context/ServiceContext.java
+++ modules/core/src/org/apache/axis/context/ServiceContext.java
@@ -31,30 +31,13 @@ import org.apache.axis.description.PhasesIncludeImpl;
 import org.apache.axis.engine.AxisFault;
 
 public class ServiceContext  extends AbstractContext implements PhasesInclude{
-    private Map operationContextMap;
-    private AxisService serviceConfig;
-    private PhasesInclude phaseInclude;
-   
+    private AxisService serviceConfig;   
 
     public ServiceContext(AxisService serviceConfig,EngineContext engineContext) {
         super(engineContext);
         this.serviceConfig = serviceConfig;
         this.operationContextMap = new HashMap();
-        phaseInclude = new PhasesIncludeImpl();
     }
-
-    public void addOperation(AxisOperation ctxt){
-        this.operationContextMap.put(ctxt.getName(),ctxt);
-    }
-
-    public AxisOperation getOperationContext(String opId){
-        return (AxisOperation)operationContextMap.get(opId);
-    }
-
-    public void removeOperationContext(AxisOperation ctxt){
-        operationContextMap.remove(ctxt.getName());
-    }
-
 
     /**
      * @return
@@ -62,49 +45,4 @@ public class ServiceContext  extends AbstractContext implements PhasesInclude{
     public AxisService getServiceConfig() {
         return serviceConfig;
     }
-    
-    public QName getName(){
-        return serviceConfig.getName();
-    }
-  
-
-    /* (non-Javadoc)
-     * @see java.lang.Object#hashCode()
-     */
-    public int hashCode() {
-        return phaseInclude.hashCode();
-    }
-
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
-    public String toString() {
-        return phaseInclude.toString();
-    }
-
-    /**
-     * @param phases
-     * @param flow
-     * @throws AxisFault
-     */
-    public void setPhases(ArrayList phases, int flow) throws AxisFault {
-        phaseInclude.setPhases(phases, flow);
-    }
-
-    /* (non-Javadoc)
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    public boolean equals(Object obj) {
-        return phaseInclude.equals(obj);
-    }
-
-    /**
-     * @param flow
-     * @return
-     * @throws AxisFault
-     */
-    public ArrayList getPhases(int flow) throws AxisFault {
-        return phaseInclude.getPhases(flow);
-    }
-
 }
