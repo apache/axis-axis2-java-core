1456d958ca03d41468b635f612b67f41536da127
Ashutosh Shahi <ashutosh@apache.org>
2005-06-06 08:43:04 +0000

    Adding Apache license to all saaj files

--- modules/saaj/test/org/apache/axis/saaj/HeadersTest.java
+++ modules/saaj/test/org/apache/axis/saaj/HeadersTest.java
@@ -1,8 +1,17 @@
 /*
- * Created on Apr 15, 2005
+ * Copyright 2004,2005 The Apache Software Foundation.
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.axis.saaj;
 
@@ -16,8 +25,6 @@ import javax.xml.soap.SOAPElement;
 /**
  * @author Ashutosh Shahi ashutosh.shahi@gmail.com
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class HeadersTest extends TestCase {
 	
