493f418a47c6be107cc32adb0ef7606d81bb319d
Nanayakkara Kuruppuge Thilina Gunarathne <thilina@apache.org>
2005-06-22 04:08:15 +0000

    changes due to merging OMBlob & OMText

--- modules/xml/src/org/apache/axis/om/impl/llom/mtom/MTOMStAXSOAPModelBuilder.java
+++ modules/xml/src/org/apache/axis/om/impl/llom/mtom/MTOMStAXSOAPModelBuilder.java
@@ -16,15 +16,7 @@
  */
 package org.apache.axis.om.impl.llom.mtom;
 
-import java.util.LinkedList;
-import java.util.ListIterator;
-
 import javax.activation.DataHandler;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.internet.MimePartDataSource;
 import javax.xml.stream.XMLStreamReader;
 
 import org.apache.axis.attachments.MIMEHelper;
@@ -40,72 +32,47 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * @author Thilina Gunarathne thilina@opensource.lk
+ * @author <a href="mailto:thilina@opensource.lk">Thilina Gunarathne </a>
  */
+
 public class MTOMStAXSOAPModelBuilder extends StAXSOAPModelBuilder {
 	private Log log = LogFactory.getLog(getClass());
-
+	
 	/**
 	 * <code>mimeHelper</code> handles deffered parsing of incoming MIME
 	 * Messages
 	 */
 	MIMEHelper mimeHelper;
-
-	LinkedList mimeBodyPartsList;
-
-	MimeMessage mimeMessage;
-
-	int partIndex = 0;
-
 	
-	public MTOMStAXSOAPModelBuilder(XMLStreamReader parser,SOAPFactory factory, MIMEHelper mimeHelper)
-	{
-		super(parser,factory);
-		this.mimeHelper=  mimeHelper;
+	int partIndex = 0;
+	
+	public MTOMStAXSOAPModelBuilder(XMLStreamReader parser,
+			SOAPFactory factory, MIMEHelper mimeHelper) {
+		super(parser, factory);
+		this.mimeHelper = mimeHelper;
 	}
 	
-
 	/**
 	 * @param reader
 	 * @param mimeHelper2
 	 */
-	public MTOMStAXSOAPModelBuilder(XMLStreamReader reader, MIMEHelper mimeHelper) {
+	public MTOMStAXSOAPModelBuilder(XMLStreamReader reader,
+			MIMEHelper mimeHelper) {
 		super(reader);
 		this.mimeHelper = mimeHelper;
 	}
-
-	/*
-	 * private XMLStreamReader getParserFromMime() throws OMException {
-	 * mimeBodyPartsList = new LinkedList(); Properties props = new
-	 * Properties(); javax.mail.Session session =
-	 * javax.mail.Session.getInstance(props, null); try { mimeMessage = new
-	 * MimeMessage(session, inStream);
-	 * 
-	 * MimeBodyPart rootMimeBodyPart;
-	 * 
-	 * rootMimeBodyPart = getRootMimeBodyPart(); return
-	 * XMLInputFactory.newInstance().createXMLStreamReader(
-	 * rootMimeBodyPart.getInputStream());
-	 *  } catch (IOException e1) { throw new OMException(e1.toString());
-	 *  } catch (MessagingException e) { throw new OMException( "Message
-	 * identified as MTOM optimised doesn't contain a valid MIME Stream" +
-	 * e.toString()); } catch (XMLStreamException e) { throw new OMException(
-	 * "SOAP Message contained within Mime root is invalid " + e.toString()); }
-	 * catch (FactoryConfigurationError e) { throw new OMException( "SOAP
-	 * Message contained within Mime root is invalid" + e.toString()); }
-	 *  }
-	 */
+	
 	protected OMNode createOMElement() throws OMException {
-
+		
 		String elementName = parser.getLocalName();
-
+		
 		String namespaceURI = parser.getNamespaceURI();
-
+		
 		// create an OMBlob if the element is an <xop:Include>
 		if (elementName.equalsIgnoreCase("Include")
 				& namespaceURI
-						.equalsIgnoreCase("http://www.w3.org/2004/08/xop/include")) {
-
+				.equalsIgnoreCase("http://www.w3.org/2004/08/xop/include")) {
+			
 			OMText node;
 			String contentID = null;
 			String contentIDName = null;
@@ -113,7 +80,7 @@ public class MTOMStAXSOAPModelBuilder extends StAXSOAPModelBuilder {
 			if (lastNode == null) {
 				// Decide whether to ckeck the level >3 or not
 				throw new OMException(
-						"XOP:Include element is not supported here");
+				"XOP:Include element is not supported here");
 			}
 			if (parser.getAttributeCount() > 0) {
 				contentID = parser.getAttributeValue(0);
@@ -124,15 +91,15 @@ public class MTOMStAXSOAPModelBuilder extends StAXSOAPModelBuilder {
 					contentID = contentID.substring(4);
 				} else {
 					throw new OMException(
-							"contentID not Found in XOP:Include element");
+					"contentID not Found in XOP:Include element");
 				}
 			} else {
 				throw new OMException(
-						"Href attribute not found in XOP:Include element");
+				"Href attribute not found in XOP:Include element");
 			}
-
+			
 			if (lastNode.isComplete()) {
-				node = new OMTextImpl(contentID, lastNode.getParent(),this);
+				node = new OMTextImpl(contentID, lastNode.getParent(), this);
 				lastNode.setNextSibling(node);
 				node.setPreviousSibling(lastNode);
 			} else {
@@ -141,7 +108,7 @@ public class MTOMStAXSOAPModelBuilder extends StAXSOAPModelBuilder {
 				e.setFirstChild(node);
 			}
 			return node;
-
+			
 		} else {
 			OMElement node;
 			if (lastNode == null) {
@@ -155,84 +122,17 @@ public class MTOMStAXSOAPModelBuilder extends StAXSOAPModelBuilder {
 				node = constructNode((OMElement) lastNode, elementName, false);
 				e.setFirstChild(node);
 			}
-
+			
 			// fill in the attributes
 			processAttributes(node);
-			log.info("Build the OMElelment {" + node.getLocalName() + '}'
-					+ node.getLocalName() + "By the StaxSOAPModelBuilder");
+			//TODO Exception when trying to log . check this
+			//			log.info("Build the OMElelment {" + node.getLocalName() + '}'
+			//					+ node.getLocalName() + "By the StaxSOAPModelBuilder");
 			return node;
 		}
 	}
-
+	
 	public DataHandler getDataHandler(String blobContentID) throws OMException {
-		/*
-		 * First checks whether the part is already parsed by checking the parts
-		 * linked list. If it is not parsed yet then call the getnextPart() till
-		 * we find the required part.
-		 */
-		MimeBodyPart mimeBodyPart;
-
-		boolean attachmentFound = false;
-		ListIterator partsIterator = mimeBodyPartsList.listIterator();
-		try {
-			while (partsIterator.hasNext()) {
-				mimeBodyPart = (MimeBodyPart) partsIterator.next();
-				if (blobContentID.equals(mimeBodyPart.getContentID())) {
-					attachmentFound = true;
-					DataHandler dh = mimeBodyPart.getDataHandler();
-					return dh;
-				}
-			}
-			while (!attachmentFound) {
-				mimeBodyPart = this.getNextMimeBodyPart();
-
-				if (mimeBodyPart == null) {
-					break;
-				}
-				String partContentID = mimeBodyPart.getContentID();
-				String delimitedBlobContentID = "<" + blobContentID + ">";
-				if (delimitedBlobContentID.equals(partContentID)) {
-					attachmentFound = true;
-					DataHandler dh = mimeBodyPart.getDataHandler();
-					return dh;
-				}
-			}
-			return null;
-		} catch (MessagingException e) {
-			throw new OMException("Invalid Mime Message " + e.toString());
-		}
-	}
-
-	private MimeBodyPart getMimeBodyPart() throws MessagingException {
-		MimeBodyPart mimeBodyPart = null;
-
-		DataHandler dh = mimeMessage.getDataHandler();
-		MimeMultipart multiPart = new MimeMultipart((MimePartDataSource) dh
-				.getDataSource());
-		mimeBodyPart = (MimeBodyPart) multiPart.getBodyPart(partIndex);
-
-		partIndex++;
-		return mimeBodyPart;
-	}
-
-	private MimeBodyPart getRootMimeBodyPart() throws MessagingException {
-		MimeBodyPart rootMimeBodyPart;
-		if (mimeBodyPartsList.isEmpty()) {
-			rootMimeBodyPart = getMimeBodyPart();
-			mimeBodyPartsList.add(rootMimeBodyPart);
-		} else {
-			rootMimeBodyPart = (MimeBodyPart) mimeBodyPartsList.getFirst();
-		}
-		return rootMimeBodyPart;
-	}
-
-	private MimeBodyPart getNextMimeBodyPart() throws MessagingException {
-		MimeBodyPart nextMimeBodyPart;
-		nextMimeBodyPart = getMimeBodyPart();
-		if (nextMimeBodyPart != null) {
-			mimeBodyPartsList.add(nextMimeBodyPart);
-			return nextMimeBodyPart;
-		} else
-			return null;
+		return mimeHelper.getDataHandler(blobContentID);
 	}
 }
