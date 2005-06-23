e3af979fd8085be7e7d69547163720e6d6b06380
Jaliya Ekanayake <jaliya@apache.org>
2005-06-23 11:01:39 +0000

    Test case for the thread pool

---
+++ modules/core/test/org/apache/axis/util/threadpool/TestThreadPool.java
@@ -0,0 +1,56 @@
+package org.apache.axis.util.threadpool;
+
+import junit.framework.TestCase;
+import org.apache.axis.engine.AxisFault;
+import org.apache.axis.AbstractTestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jaliya
+ * Date: Jun 23, 2005
+ * Time: 3:58:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestThreadPool extends AbstractTestCase {
+    /**
+     * @param testName
+     */
+    public TestThreadPool(String testName) {
+        super(testName);
+    }
+
+    class TestWorker implements AxisWorker {
+        private boolean workDone;
+
+        public void doWork() {
+            workDone = true;
+        }
+
+        public boolean isWorkDone() {
+            return workDone;
+        }
+    }
+
+
+    public void testPool() throws AxisFault {
+        ThreadPool tPool = ThreadPool.getInstance();
+        List workerList = new ArrayList();
+
+        for (int i = 0; i < 5; i++) {
+            TestWorker worker = new TestWorker();
+            workerList.add(worker);
+            tPool.addWorker(worker);
+        }
+
+        tPool.safeShutDown();
+
+        for (int i = 0; i < 5; i++) {
+            assertEquals(true, ((TestWorker) workerList.get(i)).isWorkDone());
+        }
+
+    }
+
+}
