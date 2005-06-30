8f1b9b767dcbb4748d915b77686e73eec6526def
Deepal Jayasinghe <deepal@apache.org>
2005-06-30 14:57:11 +0000

    modified the WSDL jar name

--- modules/tool/script/http-server.bat
+++ modules/tool/script/http-server.bat
@@ -5,6 +5,6 @@ rem
 rem ---------------------------------------------------------------------------
 
 rem get the classes for the simple axis server
-set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-M2.jar";"%AXIS_HOME%\lib\axis-wsdl4j-1.2-RC1.jar";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";"%AXIS_HOME%\lib\log4j-1.2.8.jar";"%AXIS_HOME%\lib\stax-1.1.1-dev.jar";"%AXIS_HOME%\lib\stax-api-1.0.jar"
+set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-M2.jar";"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";"%AXIS_HOME%\lib\log4j-1.2.8.jar";"%AXIS_HOME%\lib\stax-1.1.1-dev.jar";"%AXIS_HOME%\lib\stax-api-1.0.jar"
 
 java -cp %AXIS2_CLASS_PATH% org.apache.axis.transport.http.SimpleHTTPServer %1 %2
