fd184c8633c49707500ad44bb87f056fd27da6a6
Deepal Jayasinghe <deepal@apache.org>
2005-07-04 07:42:45 +0000

    linked following libs to bat file     geronimo-spec-activation-1.0.2-rc3.jar";     geronimo-spec-javamail-1.3.1-rc3.jar";     xbean-2.0.0-beta1.jar"

--- modules/tool/script/tcp-server.bat
+++ modules/tool/script/tcp-server.bat
@@ -5,6 +5,15 @@ rem
 rem ---------------------------------------------------------------------------
 
 rem get the classes for the simple axis server
-set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-0.9.jar";"%AXIS_HOME%\lib\axis-wsdl4j-1.2-RC1.jar";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";"%AXIS_HOME%\lib\log4j-1.2.8.jar";"%AXIS_HOME%\lib\stax-1.1.1-dev.jar";"%AXIS_HOME%\lib\stax-api-1.0.jar"
+set AXIS2_CLASS_PATH="%AXIS_HOME%";
+"%AXIS_HOME%\lib\axis2-0.9.jar";
+"%AXIS_HOME%\lib\axis-wsdl4j-1.2-RC1.jar";
+"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";
+"%AXIS_HOME%\lib\log4j-1.2.8.jar";
+"%AXIS_HOME%\lib\stax-1.1.1-dev.jar";
+"%AXIS_HOME%\lib\stax-api-1.0.jar";
+"%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc3.jar";
+"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc3.jar";
+"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar"
 
 java -cp %AXIS2_CLASS_PATH% org.apache.axis2.transport.tcp.TCPServer %1 %2
