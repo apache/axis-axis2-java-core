8160febbf5f962a4e11079df0cc889eb857cc47d
Deepal Jayasinghe <deepal@apache.org>
2005-07-04 05:10:10 +0000

    did not run in windows previosly , so I modified it to work in windows

--- modules/samples/script/amazonQS/run.bat
+++ modules/samples/script/amazonQS/run.bat
@@ -25,20 +25,7 @@ exit
 :okHome
 cd %CURRENT_DIR%
 rem get the classes for the simple axis server
-set AXIS2_CLASS_PATH="%AXIS_HOME%"
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%"
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\axis2-0.9.jar"
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar"
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar"
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\log4j-1.2.8.jar"
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\stax-1.1.1-dev.jar"
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\stax-api-1.0.jar"
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc3.jar
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar        
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc3.jar
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar
-set AXIS2_CLASS_PATH="%CLASS_PATH";"%AXIS_HOME%\lib\stax-api-1.0.jar
-
+set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-0.9.jar";"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";"%AXIS_HOME%\lib\log4j-1.2.8.jar";"%AXIS_HOME%\lib\stax-1.1.1-dev.jar";"%AXIS_HOME%\lib\stax-api-1.0.jar"
 set AXIS2_CLASS_PATH=%AXIS2_CLASS_PATH%;"%CURRENT_DIR%\amazonQS.jar"
 start javaw -cp %AXIS2_CLASS_PATH% sample.amazon.amazonSimpleQueueService.RunGUICQ
 start javaw -cp %AXIS2_CLASS_PATH% sample.amazon.amazonSimpleQueueService.RunGUIRQ
