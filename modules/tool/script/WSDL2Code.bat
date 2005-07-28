a18b4818ac1d3f4113c5651615dc17f77b21452d
Deepal Jayasinghe <deepal@apache.org>
2005-07-28 12:52:55 +0000

    fixing BUG 105

--- modules/tool/script/WSDL2Code.bat
+++ modules/tool/script/WSDL2Code.bat
@@ -19,7 +19,7 @@ exit
 
 :okHome
 rem set the classes
-set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-0.9.jar";"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";"%AXIS_HOME%\lib\log4j-1.2.8.jar";"%AXIS_HOME%\lib\stax-1.1.1-dev.jar";"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar";"%AXIS_HOME%\lib\stax-api-1.0.jar""%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc3.jar";"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc3.jar";"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar"
+set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-0.9.jar";"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";"%AXIS_HOME%\lib\log4j-1.2.8.jar";"%AXIS_HOME%\lib\stax-1.1.1-dev.jar";"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar";"%AXIS_HOME%\lib\stax-api-1.0.jar";"%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc3.jar";"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc3.jar";"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar"
 
 java -cp %AXIS2_CLASS_PATH% org.apache.axis2.wsdl.WSDL2Code %1 %2 %3 %4 %5 %6 %7 %8 %9 
 
