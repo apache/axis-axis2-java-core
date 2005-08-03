65f100968f7d78620db9725ef2800dcbf3e11797
Hemapani Srinath Perera <hemapani@apache.org>
2005-08-03 12:54:35 +0000

    forget to add the class, checked in

---
+++ modules/core/src/org/apache/axis2/RESTCall.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.axis2.clientapi;
+
+import org.apache.axis2.om.OMElement;
+import org.apache.axis2.om.OMAbstractFactory;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.context.ServiceContext;
+
+
+public class RESTCall extends Call {
+
+    public RESTCall() throws AxisFault {
+        super();
+    }
+
+    public RESTCall(ServiceContext service) throws AxisFault {
+        super(service);
+    }
+
+    public OMElement invokeBlocking()
+            throws AxisFault {
+        return super.invokeBlocking(
+                "nothing",
+                OMAbstractFactory.getOMFactory().createOMElement(
+                        "nothing", "nothing", "nothing"));
+
+    }
+
+    public void invokeNonBlocking(Callback callback)
+            throws AxisFault {
+        super.invokeNonBlocking(
+                "nothing",
+                OMAbstractFactory.getOMFactory().createOMElement(
+                        "nothing", "nothing", "nothing"), callback);
+
+    }
+}
