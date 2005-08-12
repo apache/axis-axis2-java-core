9da21fcad8a344db8a835bd4c48506d577c25249
Ajith Ranabahu <ajith@apache.org>
2005-08-12 13:31:58 +0000

    Updating the scripts. Shell scripts by Courtesy of Saminda :)

--- modules/samples/script/googleSearch/run.bat
+++ modules/samples/script/googleSearch/run.bat
@@ -18,7 +18,7 @@ set AXIS_HOME=%cd%
 echo using Axis Home %AXIS_HOME%
 
 :gotHome
-if EXIST "%AXIS_HOME%\lib\axis2-0.9.jar" goto okHome
+if EXIST "%AXIS_HOME%\lib\axis2-0.91.jar" goto okHome
 echo The AXIS_HOME environment variable seems not to point to the correct loaction!
 echo This environment variable is needed to run this program
 pause
@@ -27,7 +27,24 @@ exit
 :okHome
 rem set the classes
 cd %CURRENT_DIR%
-set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-0.9.jar";"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";"%AXIS_HOME%\lib\log4j-1.2.8.jar";"%AXIS_HOME%\lib\stax-1.1.1-dev.jar";"%AXIS_HOME%\lib\stax-api-1.0.jar";"%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc3.jar";"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc3.jar";"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar"
+set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-0.91.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-logging-1.0.3.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\log4j-1.2.8.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\stax-1.1.1-dev.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\stax-api-1.0.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc4.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc5.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-httpclient-3.0-rc2.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\ant-1.6.2.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-fileupload-1.0.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\geronimo-spec-servlet-2.4-rc4.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\groovy-all-1.0-jsr-01.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\jaxen-1.1-beta-7.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\log4j-1.2.8.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\XmlSchema-0.9.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\xmlunit-1.0.jar"
 set AXIS2_CLASS_PATH=%AXIS2_CLASS_PATH%;"%CURRENT_DIR%\googleSearch.jar"
 start javaw -cp %AXIS2_CLASS_PATH% sample.google.search.AsynchronousClient
 
--- modules/samples/script/googleSpellcheck/run.bat
+++ modules/samples/script/googleSpellcheck/run.bat
@@ -10,7 +10,7 @@ set CURRENT_DIR=%cd%
 rem check the AXIS_HOME environment variable
 if not "%AXIS_HOME%" == "" goto gotHome
 
-rem guess the home. Jump two directories up nad take that as the home
+rem guess the home. Jump two directories up and take that as the home
 cd ..
 cd ..
 set AXIS_HOME=%cd%
@@ -19,7 +19,7 @@ echo using Axis Home %AXIS_HOME%
 
 
 :gotHome
-if EXIST "%AXIS_HOME%\lib\axis2-0.9.jar" goto okHome
+if EXIST "%AXIS_HOME%\lib\axis2-0.91.jar" goto okHome
 echo The AXIS_HOME environment variable seems not to point to the correct loaction!
 echo This environment variable is needed to run this program
 pause
@@ -28,8 +28,28 @@ exit
 :okHome
 cd %CURRENT_DIR%
 rem get the classes
-set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-0.9.jar";"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";"%AXIS_HOME%\lib\log4j-1.2.8.jar";"%AXIS_HOME%\lib\stax-1.1.1-dev.jar";"%AXIS_HOME%\lib\stax-api-1.0.jar";"%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc3.jar";"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc3.jar";"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar"
+set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-0.91.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-logging-1.0.3.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\log4j-1.2.8.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\stax-1.1.1-dev.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\stax-api-1.0.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc4.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc5.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-httpclient-3.0-rc2.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\ant-1.6.2.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-fileupload-1.0.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\geronimo-spec-servlet-2.4-rc4.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\groovy-all-1.0-jsr-01.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\jaxen-1.1-beta-7.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\log4j-1.2.8.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\XmlSchema-0.9.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\xmlunit-1.0.jar"
+
 set AXIS2_CLASS_PATH=%AXIS2_CLASS_PATH%;"%CURRENT_DIR%\googleSpellcheck.jar"
-start javaw -cp %AXIS2_CLASS_PATH% sample.google.spellcheck.SuggestionForm
+
+rem start javaw -cp %AXIS2_CLASS_PATH% sample.google.spellcheck.SuggestionForm
+java -cp %AXIS2_CLASS_PATH% sample.google.spellcheck.SuggestionForm
 
 :end
