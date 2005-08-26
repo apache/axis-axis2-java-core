3d47ea26663a3591f3280da1d8942095b8e346c4
Davanum Srinivas <dims@apache.org>
2005-08-26 04:53:51 +0000

    Cleanup test case

--- modules/xml/test/org/apache/axis2/om/infoset/XMLConformanceTest.java
+++ modules/xml/test/org/apache/axis2/om/infoset/XMLConformanceTest.java
@@ -15,130 +15,38 @@
 */
 package org.apache.axis2.om.infoset;
 
-import org.apache.axis2.om.OMAbstractFactory;
-import org.apache.axis2.om.OMElement;
-import org.apache.axis2.om.impl.llom.builder.StAXOMBuilder;
-import org.apache.axis2.om.impl.llom.factory.OMXMLBuilderFactory;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.custommonkey.xmlunit.Diff;
-import org.custommonkey.xmlunit.XMLTestCase;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 
-public class XMLConformanceTest extends XMLTestCase {
-    private static int successCount = 0;
-    private static int parsedCount = 0;
-    private static int fileCount = 0;
-    private Log log = LogFactory.getLog(getClass());
+public class XMLConformanceTest extends TestCase {
 
-    public XMLConformanceTest(String name) {
-        super(name);
-    }
+    public static Test suite() throws Exception {
+        TestSuite suite = new TestSuite();
 
-    public void setUp() {
-    }
-
-    public void testXMLConformance() throws Exception {
-        //The 'testSuiteDirectory' value can also be a specific file location
-        //value, needn't necessarily be a directory
         File testSuiteDirectory = new File("test-resources/XMLSuite/xmlconf");
-        if(testSuiteDirectory.exists()) {
-            ProcessDir(testSuiteDirectory);
-            log.info("File count is " + fileCount);
-            log.info("Parsed count is " + parsedCount +
-                    ". This is just partial success");
-            log.info("Complete success count is " + successCount);
-        } else {
-            log.info("Skipping W3C XMLSuite test");
+        if (testSuiteDirectory.exists()) {
+            ProcessDir(testSuiteDirectory, suite);
         }
+        return suite;
     }
 
-    public void ProcessDir(File dir) throws Exception {
+    private static void ProcessDir(File dir, TestSuite suite) throws Exception {
         if (dir.isDirectory()) {
             //process all children
             String[] children = dir.list();
             for (int i = 0; i < children.length; i++) {
                 File child = (new File(dir, children[i]));
-                ProcessDir(child);
+                ProcessDir(child, suite);
             }
-        } else { //meaning you got a file
+        } else {
             //check if it's xml file
             String absPath = dir.getAbsolutePath();
             if (absPath.endsWith(".xml")) {
-                //process it
-                testSingleFileConformance(absPath);
-                fileCount++;
-            } else {
-                //ignore non .xml files
+                suite.addTest(new XMLConformanceUnit(absPath, "testSingleFileConformance"));
             }
         }
-    }
-
-    public void testSingleFileConformance(String absolutePath)
-            throws Exception {
-        OMElement rootElement;
-        //fileCount++;
-        //get a stax om builder
-        try {
-            StAXOMBuilder staxOMBuilder = OMXMLBuilderFactory.
-                    createStAXOMBuilder(OMAbstractFactory.getOMFactory(),
-                            XMLInputFactory.newInstance().createXMLStreamReader(
-                                    new FileInputStream(absolutePath), "UTF-8"));
-            rootElement = staxOMBuilder.getDocumentElement();
-        } catch (Exception e) {
-            log.info("Exception trying to get hold of rootElement: "
-                    + e.getMessage());
-            log.info("in file: " + absolutePath + "\n");
-            return;
-        }
-        //we will write output into the file named TempOutputFile.txt in
-        //current directory
-        String tempFile = "TempOutputFile.txt";
-        XMLStreamWriter writer;
-        try {
-            writer = XMLOutputFactory.newInstance().
-                    createXMLStreamWriter(new FileOutputStream(tempFile));
-            rootElement.serializeWithCache(writer);
-        } catch (XMLStreamException e) {
-            log.info("Error in creating XMLStreamWriter to write parsed xml into");
-            return;
-        } catch (Exception e) {
-            log.info("Exception while serializing: " +
-                    e.getMessage());
-            log.info("in file: " + absolutePath + "\n");
-            return;
-        }
-        writer.flush();
-        writer.close();
-        parsedCount++;
-        //Comparing the equality of the TempOutputFile.txt and the input xml is due
-        Diff diff;
-        try {
-            diff = compareXML(new FileReader(absolutePath), new FileReader(
-                    "TempOutputFile.txt"));
-        } catch (Exception e) {
-            log.info("" +
-                    "Error comparing original and generated files for: " +
-                    absolutePath);
-            log.info("Error message is: " + e.getMessage());
-            return;
-        }
-        try {
-            assertXMLEqual(diff, true);
-            successCount++;
-        } catch (Error e) {
-            log.info("XMLEquality failed for file: " + absolutePath);
-        }
-    }
-
-    public void tearDown() {
     }
 }
