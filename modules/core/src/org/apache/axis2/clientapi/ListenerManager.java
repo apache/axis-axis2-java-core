eed53e17dcfe1c85bae812e7e59a36bbdb8c9b38
Deepal Jayasinghe <deepal@apache.org>
2005-09-06 10:52:48 +0000

    There was a bug when creating ServerSocket , for loop did not work properly

--- modules/core/src/org/apache/axis2/clientapi/ListenerManager.java
+++ modules/core/src/org/apache/axis2/clientapi/ListenerManager.java
@@ -125,7 +125,7 @@ public class ListenerManager {
             try {
                 return new ServerSocket(port + i);
             } catch (IOException e) {
-                throw new AxisFault(Messages.getMessage("failedToOpenSocket"), e);
+              //  throw new AxisFault(Messages.getMessage("failedToOpenSocket"), e);
             }
         }
         throw new AxisFault(Messages.getMessage("failedToOpenSocket"));
