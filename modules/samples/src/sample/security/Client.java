60dfb6403520dbc316d31aa7d288368648413230
Ruchith Udayanga Fernando <ruchithf@apache.org>
2005-09-16 16:15:09 +0000

    Sample using the wss4j module - completed

--- modules/samples/src/sample/security/Client.java
+++ modules/samples/src/sample/security/Client.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2004,2005 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package sample.security;
 
 import java.io.StringWriter;
@@ -19,20 +35,20 @@ public class Client {
 	/**
 	 * @param args
 	 */
-	private static EndpointReference targetEPR = new EndpointReference(
-			"http://127.0.0.1:8080/axis2/services/SecureService/echo");
-
 	public static void main(String[] args) {
 		try {
 			
-			//TODO : Get the repository location from the args
-			
+			// Get the repository location from the args
+			String repo = args[0];
+			String port = args[1];
 			
 			OMElement payload = getEchoElement();
-			Call call = new Call();
-			call.setTo(targetEPR);
+			Call call = new Call(repo);
+			
+			call.setTo(new EndpointReference("http://127.0.0.1:" + port + "/axis2/services/SecureService/echo"));
 			call.setTransportInfo(Constants.TRANSPORT_HTTP,
 					Constants.TRANSPORT_HTTP, false);
+			call.set(Constants.Configuration.ENABLE_MTOM, Constants.VALUE_TRUE);
 
 			//Blocking invocation
 			OMElement result = call.invokeBlocking("echo", payload);
@@ -42,8 +58,9 @@ public class Client {
 					.createXMLStreamWriter(writer));
 			writer.flush();
 
-			System.out.println(writer.toString());
+			System.out.println("Response: " + writer.toString());
 
+			System.out.println("SecureService Invocation successful :-)");
 		} catch (AxisFault axisFault) {
 			axisFault.printStackTrace();
 		} catch (XMLStreamException e) {
