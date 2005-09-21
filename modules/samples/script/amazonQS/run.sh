20f831b0f2021e769b0c9ba7c99c178822d0f628
Ruchith Udayanga Fernando <ruchithf@apache.org>
2005-09-21 06:42:21 +0000

    Changed the shell scripts to pickup all the jars in the lib directory in the bin distribution. Now we don't have to change the shell scripts everytime a jar version changes

--- modules/samples/script/amazonQS/run.sh
+++ modules/samples/script/amazonQS/run.sh
@@ -1,23 +1,13 @@
-export AXIS2_HOME=../..
-AXIS2_CLASSPATH=$AXIS2_HOME/lib/axis2-1.0-alpha.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/axis-wsdl4j-1.2.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/commons-logging-1.0.3.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/log4j-1.2.8.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/stax-1.1.2-dev.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/stax-api-1.0.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/geronimo-spec-activation-1.0.2-rc4.jar   
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/commons-codec-1.3.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/commons-httpclient-3.0-rc3.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/geronimo-spec-javamail-1.3.1-rc5.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/groovy-all-1.0-jsr-01.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/jaxen-1.1-beta-7.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/xbean-2.0.0-beta1.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/XmlSchema-0.9.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$AXIS2_HOME/lib/xmlunit-1.0.jar
+#!/bin/sh
 
+export AXIS2_HOME=../..
 AXIS2_CLASSPATH=$AXIS2_CLASSPATH:amazonQS.jar
+for f in $AXIS2_HOME/lib/*.jar
+do
+  AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$f
+done
 export AXIS2_CLASSPATH
-echo the classpath $AXIS2_CLASSPATH
+echo classpath: $AXIS2_CLASSPATH
 java -classpath $AXIS2_CLASSPATH sample.amazon.amazonSimpleQueueService.RunGUICQ &
 java -classpath $AXIS2_CLASSPATH sample.amazon.amazonSimpleQueueService.RunGUIRQ &
 
