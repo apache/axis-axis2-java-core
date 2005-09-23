3e658c9887dd17eb5135f566f1200d5a6f59031e
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2005-09-23 10:33:07 +0000

    Scripts for tools being changed

--- modules/tool/script/http-server.sh
+++ modules/tool/script/http-server.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-cd ..
-AXIS2_CLASSPATH=./lib/axis2-0.91.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/axis-wsdl4j-1.2.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/commons-logging-1.0.3.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/log4j-1.2.8.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/stax-1.1.2-dev.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/geronimo-spec-activation-1.0.2-rc3.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/axis-wsdl4j-1.2.jar        
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/geronimo-spec-javamail-1.3.1-rc3.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/xbean-2.0.0-beta1.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/stax-api-1.0.jar
-AXIS2_CLASSPATH=$AXIS2_CLASSPATH:./lib/commons-codec-1.3.jar
+export AXIS2_HOME=..
 
+for f in $AXIS2_HOME/lib/*.jar
+do
+  AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$f
+done
 export AXIS2_CLASSPATH
+
 echo the classpath $AXIS2_CLASSPATH
 java -classpath $AXIS2_CLASSPATH org.apache.axis2.transport.http.SimpleHTTPServer $1 $2
--- modules/tool/script/tcp-server.bat
+++ modules/tool/script/tcp-server.bat
@@ -4,7 +4,45 @@ rem Start script for running the Simple Axis Server
 rem
 rem ---------------------------------------------------------------------------
 
-rem get the classes for the simple axis server
-set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-0.91.jar";"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar";"%AXIS_HOME%\lib\commons-logging-1.0.3.jar";"%AXIS_HOME%\lib\log4j-1.2.8.jar";"%AXIS_HOME%\lib\stax-1.1.2-dev.jar";"%AXIS_HOME%\lib\stax-api-1.0.jar";"%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc3.jar";"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc3.jar";"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar";%AXIS_HOME%\lib\commons-codec-1.3.jar
+rem store the current directory
+set CURRENT_DIR=%cd%
+
+rem check the AXIS_HOME environment variable
+if not "%AXIS_HOME%" == "" goto gotHome
+
+rem guess the home. Jump two directories up nad take that as the home
+cd ..
+cd ..
+set AXIS_HOME=%cd%
+
+:gotHome
+if EXIST "%AXIS_HOME%\lib\axis2-1.0-alpha.jar" goto okHome
+echo The AXIS_HOME environment variable seems not to point to the correct location!
+echo This environment variable is needed to run this program
+pause
+exit
+
+:okHome
+rem set the classes
+cd %CURRENT_DIR%
+set AXIS2_CLASS_PATH="%AXIS_HOME%";"%AXIS_HOME%\lib\axis2-1.0-alpha.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\axis-wsdl4j-1.2.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-logging-1.0.3.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\log4j-1.2.8.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\stax-1.1.2-dev.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\stax-api-1.0.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\geronimo-spec-activation-1.0.2-rc4.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\geronimo-spec-javamail-1.3.1-rc5.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\xbean-2.0.0-beta1.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-httpclient-3.0-rc3.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-codec-1.3.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\ant-1.6.2.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\commons-fileupload-1.0.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\geronimo-spec-servlet-2.4-rc4.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\groovy-all-1.0-jsr-01.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\jaxen-1.1-beta-7.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\log4j-1.2.8.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\XmlSchema-0.9.jar"
+set AXIS2_CLASS_PATH= %AXIS2_CLASS_PATH%;"%AXIS_HOME%\lib\xmlunit-1.0.jar"
 
 java -cp %AXIS2_CLASS_PATH% org.apache.axis2.transport.tcp.TCPServer %1 %2
