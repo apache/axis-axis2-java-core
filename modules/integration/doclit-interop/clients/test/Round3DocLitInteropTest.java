0b67e06ec4840365c769fe5d0893ca0245ab2fe4
Deepal Jayasinghe <deepal@apache.org>
2005-09-27 11:53:35 +0000

    Applying Gayans patch , Interop testings. It seems that he has done a great job.

---
+++ modules/integration/doclit-interop/clients/test/Round3DocLitInteropTest.java
@@ -0,0 +1,38 @@
+//package test;
+//
+//import junit.framework.TestCase;
+//import org.soapinterop.xsd.Document;
+//import org.soapinterop.xsd.XDocumentDocument1;
+//import org.soapinterop.xsd.Person;
+//import org.soapinterop.xsd.XPersonDocument;
+//import test.stub1.SoapInteropCompound1PortTypeStub;
+//
+///**
+// * Created by IntelliJ IDEA.
+// * User: Gayan
+// * Date: Sep 15, 2005
+// * Time: 11:58:15 AM
+// * To change this template use File | Settings | File Templates.
+// */
+//public class Round3DocLitInteropTest extends TestCase{
+//
+//    public void testTest1() throws Exception{
+//        SoapInteropCompound1PortTypeStub stub = new SoapInteropCompound1PortTypeStub();
+//            Document xDoc = Document.Factory.newInstance();
+//            xDoc.setID("123");
+//            xDoc.setStringValue("Gayan Asanka");
+//            XDocumentDocument1 doc = XDocumentDocument1.Factory.newInstance();
+//            doc.setXDocument(xDoc);
+//            System.out.println( stub.echoDocument(doc));
+//
+//            Person pers = Person.Factory.newInstance();
+//            pers.setName("Gayan Asanka");
+//            pers.setAge(28);
+//            pers.setID((float)123.456);
+//            pers.setMale(true);
+//            XPersonDocument xPersDoc = XPersonDocument.Factory.newInstance();
+//            xPersDoc.setXPerson(pers);
+//            System.out.println(stub.echoPerson(xPersDoc));
+//    }
+//
+//}
---
+++ modules/integration/doclit-interop/clients/test/Round3DoclitHeadersInteropTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import test.stub.RetHeaderPortTypeStub;
+import test.stub.databinding.org.soapinterop.*;
+import org.apache.axis2.AxisFault;
+import junit.framework.TestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gayan
+ * Date: Sep 22, 2005
+ * Time: 4:03:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Round3DoclitHeadersInteropTest extends TestCase{
+
+    RetHeaderPortTypeStub stub = null;
+    Header2 h2 = null;
+    Header2Document h2Doc = null;
+    Header1 h1 = null;
+    Header1Document h1Doc = null;
+    EchoStringParamDocument paraDoc = null;
+    String str = "String Parameter";
+
+
+    public void testEchoString() throws Exception{
+        stub = new RetHeaderPortTypeStub();
+        h2 = Header2.Factory.newInstance();
+        h2.setInt(456);
+        h2.setString("Header2 para");
+        h2Doc = Header2Document.Factory.newInstance();
+        h2Doc.setHeader2(h2);
+        h1 = Header1.Factory.newInstance();
+        h1.setInt(123);
+        h1.setString("string header1 para");
+        h1Doc = Header1Document.Factory.newInstance();
+        h1Doc.setHeader1(h1);
+        paraDoc = EchoStringParamDocument.Factory.newInstance();
+        paraDoc.setEchoStringParam(str);
+        EchoStringReturnDocument retDoc = stub.echoString(paraDoc, h1Doc, h2Doc);
+        assertEquals(str,retDoc.getEchoStringReturn());
+    }
+}
