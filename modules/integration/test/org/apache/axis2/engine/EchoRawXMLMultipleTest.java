462178042e8b7a0fbf9d33e916b0dc79dceafaac
Saminda Wishwajith Abeyruwan <saminda@apache.org>
2005-10-20 08:39:03 +0000

    This test case forcuses on creating multiple Call objects and invoke, invokeblocking,  invokNonblocking with  sync/async/duel configurations.

---
+++ modules/integration/test/org/apache/axis2/engine/EchoRawXMLMultipleTest.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.axis2.engine;
+
+import org.apache.axis2.addressing.EndpointReference;
+import org.apache.axis2.integration.UtilServer;
+import org.apache.axis2.integration.TestingUtils;
+import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.ServiceContext;
+import org.apache.axis2.description.ServiceDescription;
+import org.apache.axis2.util.Utils;
+import org.apache.axis2.om.OMElement;
+import org.apache.axis2.om.OMAbstractFactory;
+import org.apache.axis2.Constants;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.soap.SOAPFactory;
+import org.apache.axis2.clientapi.Callback;
+import org.apache.axis2.clientapi.AsyncResult;
+import org.apache.axis2.clientapi.Call;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.xml.namespace.QName;
+
+import junit.framework.TestCase;
+
+
+public class EchoRawXMLMultipleTest extends TestCase {
+    protected EndpointReference targetEPR =
+            new EndpointReference("http://127.0.0.1:"
+                    + (UtilServer.TESTING_PORT)
+                    + "/axis/services/EchoXMLService/echoOMElement");
+    protected Log log = LogFactory.getLog(getClass());
+    protected QName serviceName = new QName("EchoXMLService");
+    protected QName operationName = new QName("echoOMElement");
+    protected QName transportName = new QName("http://localhost/my",
+            "NullTransport");
+
+    protected AxisConfiguration engineRegistry;
+    protected MessageContext mc;
+    private ConfigurationContext config;
+    protected ServiceContext serviceContext;
+    protected ServiceDescription service;
+
+    protected boolean finish = false;
+
+    public EchoRawXMLMultipleTest() {
+        super(EchoRawXMLTest.class.getName());
+    }
+
+    public EchoRawXMLMultipleTest(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws Exception {
+        UtilServer.start();
+        service =
+                Utils.createSimpleService(serviceName,
+                        Echo.class.getName(),
+                        operationName);
+        UtilServer.deployService(service);
+    }
+
+    protected void tearDown() throws Exception {
+        UtilServer.unDeployService(serviceName);
+        UtilServer.stop();
+        UtilServer.unDeployClientService();
+    }
+
+
+    public void testEchoXMLMultipleASync() throws Exception {
+        OMElement payload = TestingUtils.createDummyOMElement();
+
+        for (int i = 0; i < 5; i++) {
+            org.apache.axis2.clientapi.Call call =
+                    new org.apache.axis2.clientapi.Call(
+                            "target/test-resources/intregrationRepo");
+
+            call.setTo(targetEPR);
+            call.setTransportInfo(Constants.TRANSPORT_HTTP,
+                    Constants.TRANSPORT_HTTP,
+                    false);
+
+            Callback callback = new Callback() {
+                public void onComplete(AsyncResult result) {
+                    TestingUtils.campareWithCreatedOMElement(
+                            result.getResponseEnvelope()
+                                    .getBody().getFirstElement());
+                    finish = true;
+                }
+
+                public void reportError(Exception e) {
+                    log.info(e.getMessage());
+                    finish = true;
+                }
+            };
+
+            call.invokeNonBlocking(operationName.getLocalPart(),
+                    payload,
+                    callback);
+            int index = 0;
+            while (!finish) {
+                Thread.sleep(1000);
+                index++;
+                if (index > 10) {
+                    throw new AxisFault(
+                            "Server is shutdown as the Async response take too longs time");
+                }
+            }
+            call.close();
+        }
+
+
+        log.info("send the request");
+    }
+
+    public void testEchoXMLMultipleDuelASync() throws Exception {
+        OMElement payload = TestingUtils.createDummyOMElement();
+
+        for (int i = 0; i < 5; i++) {
+            org.apache.axis2.clientapi.Call call =
+                    new org.apache.axis2.clientapi.Call(
+                            "target/test-resources/intregrationRepo");
+
+            call.setTo(targetEPR);
+            call.setTransportInfo(Constants.TRANSPORT_HTTP,
+                    Constants.TRANSPORT_HTTP,
+                    true);
+
+            Callback callback = new Callback() {
+                public void onComplete(AsyncResult result) {
+                    TestingUtils.campareWithCreatedOMElement(
+                            result.getResponseEnvelope()
+                                    .getBody().getFirstElement());
+                    finish = true;
+                }
+
+                public void reportError(Exception e) {
+                    log.info(e.getMessage());
+                    finish = true;
+                }
+            };
+
+            call.invokeNonBlocking(operationName.getLocalPart(),
+                    payload,
+                    callback);
+            int index = 0;
+            while (!finish) {
+                Thread.sleep(1000);
+                index++;
+                if (index > 10) {
+                    throw new AxisFault(
+                            "Server is shutdown as the Async response take too longs time");
+                }
+            }
+            call.close();
+        }
+
+
+        log.info("send the request");
+    }
+
+    public void testEchoXMLMultipleSync() throws Exception {
+        SOAPFactory fac = OMAbstractFactory.getSOAP11Factory();
+
+        OMElement payload = TestingUtils.createDummyOMElement();
+        for (int i = 0; i < 5; i++) {
+            org.apache.axis2.clientapi.Call call =
+                    new org.apache.axis2.clientapi.Call(
+                            "target/test-resources/intregrationRepo");
+
+            call.setTo(targetEPR);
+            call.setTransportInfo(Constants.TRANSPORT_HTTP,
+                    Constants.TRANSPORT_HTTP,
+                    false);
+
+            OMElement result =
+                    call.invokeBlocking(operationName.getLocalPart(),
+                            payload);
+            TestingUtils.campareWithCreatedOMElement(result);
+            call.close();
+        }
+    }
+    public void testEchoXMLMultipleDuelSync() throws Exception {
+        SOAPFactory fac = OMAbstractFactory.getSOAP11Factory();
+
+        OMElement payload = TestingUtils.createDummyOMElement();
+        for (int i = 0; i < 5; i++) {
+            org.apache.axis2.clientapi.Call call =
+                    new org.apache.axis2.clientapi.Call(
+                            "target/test-resources/intregrationRepo");
+
+            call.setTo(targetEPR);
+            call.setTransportInfo(Constants.TRANSPORT_HTTP,
+                    Constants.TRANSPORT_HTTP,
+                    true);
+
+            OMElement result =
+                    call.invokeBlocking(operationName.getLocalPart(),
+                            payload);
+            TestingUtils.campareWithCreatedOMElement(result);
+            call.close();
+        }
+    }
+
+
+
+}
