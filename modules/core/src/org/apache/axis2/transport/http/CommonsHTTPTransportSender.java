88e321865e7a09d72c120678cde72e672cb70098
Eran Chinthaka <chinthaka@apache.org>
2005-11-07 07:14:47 +0000

    making action to be included within double quotes.

--- modules/core/src/org/apache/axis2/transport/http/CommonsHTTPTransportSender.java
+++ modules/core/src/org/apache/axis2/transport/http/CommonsHTTPTransportSender.java
@@ -375,7 +375,7 @@ public class CommonsHTTPTransportSender
 
             // action header is not mandated in SOAP 1.2. So putting it, if available
             if (!msgCtxt.isSOAP11() && soapActionString != null && !"".equals(soapActionString.trim())) {
-                contentType = contentType + ";action=" + soapActionString + ";";
+                contentType = contentType + ";action=\"" + soapActionString + "\";";
             }
             return contentType;
         }
