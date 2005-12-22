f4b61b02baf720a38b21ba7f76398bbbdcdf2079
Davanum Srinivas <dims@apache.org>
2005-12-22 05:40:02 +0000

    fix comment headers in generated code

--- modules/codegen/src/org/apache/axis2/wsdl/codegen/writer/PrettyPrinter.java
+++ modules/codegen/src/org/apache/axis2/wsdl/codegen/writer/PrettyPrinter.java
@@ -21,6 +21,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
@@ -48,6 +49,17 @@ public class PrettyPrinter {
             // Set the output file
             Method output = clazz.getMethod("setOutput", new Class[]{File.class});
             output.invoke(prettifier, new Object[]{file});
+
+            Class clazz2 = Loader.loadClass("de.hunsicker.jalopy.storage.Convention");
+            Method instance = clazz2.getMethod("getInstance", new Class[]{});
+            Object settings = instance.invoke(null, new Object[]{});
+
+            Class clazz3 = Loader.loadClass("de.hunsicker.jalopy.storage.ConventionKeys");
+            Field field = clazz3.getField("COMMENT_JAVADOC_PARSE");
+            Object key = field.get(null);
+            
+            Method put = clazz2.getMethod("put", new Class[]{ key.getClass(), String.class});
+            put.invoke(settings, new Object[]{key, "true"});
 
             // format and overwrite the given input file
             Method format = clazz.getMethod("format", new Class[]{});
