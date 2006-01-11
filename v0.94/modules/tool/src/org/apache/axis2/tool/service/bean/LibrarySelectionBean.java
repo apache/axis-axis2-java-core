3add5fbbbe913ab97622a564404e9463ca9145ea
Eran Chinthaka <chinthaka@apache.org>
2006-01-11 14:50:44 +0000

    Axis2 0.94 release on 11-01.2006.

---
+++ v0.94/modules/tool/src/org/apache/axis2/tool/service/bean/LibrarySelectionBean.java
@@ -0,0 +1,22 @@
+
+package org.apache.axis2.tool.service.bean;
+
+public class LibrarySelectionBean {
+    
+    private String[] fileList = null;
+    
+    
+
+    /**
+     * @return Returns the fileList.
+     */
+    public String[] getFileList() {
+        return fileList;
+    }
+    /**
+     * @param fileList The fileList to set.
+     */
+    public void setFileList(String[] fileList) {
+        this.fileList = fileList;
+    }
+}
