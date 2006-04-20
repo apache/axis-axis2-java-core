94960543be2870847b1a37ec4837c5ecf5714132
Ruchith Udayanga Fernando <ruchithf@apache.org>
2006-04-20 04:53:14 +0000

    we should use the qulified name when calling createElementNS()

--- modules/saaj/src/org/apache/axis2/saaj/SOAPBodyImpl.java
+++ modules/saaj/src/org/apache/axis2/saaj/SOAPBodyImpl.java
@@ -125,9 +125,9 @@ public class SOAPBodyImpl extends SOAPElementImpl implements SOAPBody {
         if (omSOAPBody.hasFault()) {
             throw new SOAPException("A SOAPFault has been already added to this SOAPBody");
         }
-        SOAPBodyElementImpl childEle =
-                new SOAPBodyElementImpl((ElementImpl) getOwnerDocument().createElementNS(uri,
-                                                                                         localName));
+        SOAPBodyElementImpl childEle = new SOAPBodyElementImpl(
+                (ElementImpl) getOwnerDocument().createElementNS(uri,
+                        prefix + ":" + localName));
         childEle.element.setUserData(SAAJ_NODE, childEle, null);
         childEle.element.setNamespace(childEle.element.declareNamespace(uri, prefix));
         element.appendChild(childEle.element);
