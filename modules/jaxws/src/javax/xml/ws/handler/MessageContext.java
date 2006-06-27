2e791dd526d477515ec3474e114ee51152982ce0
Davanum Srinivas <dims@apache.org>
2006-06-27 15:31:33 +0000

    Fix for AXIS2-855 -  javax.xml.ws.handler.MessageContext constant fields need update

--- modules/jaxws/src/javax/xml/ws/handler/MessageContext.java
+++ modules/jaxws/src/javax/xml/ws/handler/MessageContext.java
@@ -28,7 +28,8 @@ public interface MessageContext
     public abstract Scope getScope(String s);
 
     public static final String MESSAGE_OUTBOUND_PROPERTY = "javax.xml.ws.handler.message.outbound";
-    public static final String MESSAGE_ATTACHMENTS = "javax.xml.ws.binding.attachments";
+    public static final String INBOUND_MESSAGE_ATTACHMENTS="javax.xml.ws.binding.attachments.inbound";
+    public static final String OUTBOUND_MESSAGE_ATTACHMENTS = "javax.xml.ws.binding.attachments.outbound";
     public static final String WSDL_DESCRIPTION = "javax.xml.ws.wsdl.description";
     public static final String WSDL_SERVICE = "javax.xml.ws.wsdl.service";
     public static final String WSDL_PORT = "javax.xml.ws.wsdl.port";
@@ -36,6 +37,8 @@ public interface MessageContext
     public static final String WSDL_OPERATION = "javax.xml.ws.wsdl.operation";
     public static final String HTTP_RESPONSE_CODE = "javax.xml.ws.http.response.code";
     public static final String HTTP_REQUEST_HEADERS = "javax.xml.ws.http.request.headers";
+    public static final String PATH_INFO = "javax.xml.ws.http.request.pathinfo";
+    public static final String QUERY_STRING = "javax.xml.ws.http.request.querystring";
     public static final String HTTP_RESPONSE_HEADERS = "javax.xml.ws.http.response.headers";
     public static final String HTTP_REQUEST_METHOD = "javax.xml.ws.http.request.method";
     public static final String SERVLET_REQUEST = "javax.xml.ws.servlet.request";
