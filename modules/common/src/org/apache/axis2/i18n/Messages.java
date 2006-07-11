d0ed8839eacb208a0be26471dace226b3f2b0fe0
Chamikara Madhusanka Jayalath <chamikara@apache.org>
2006-07-11 10:12:51 +0000

    Did corrections to the Message class to maintain a Map of MessageBundles. Extension modules will be add their own MessageBundle here and will be able to get the values through newly added set of getMessageFromBundle methods.

--- modules/common/src/org/apache/axis2/i18n/Messages.java
+++ modules/common/src/org/apache/axis2/i18n/Messages.java
@@ -16,10 +16,10 @@
 
 package org.apache.axis2.i18n;
 
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-
 
 public class Messages {
     private static final Class thisClass = Messages.class;
@@ -28,6 +28,9 @@ public class Messages {
 
     private static final String resourceName = MessagesConstants.resourceName;
     private static final Locale locale = MessagesConstants.locale;
+    
+    public static final String DEFAULT_MESSAGE_BUNDLE_KEY = "default";
+    private static final String NO_MESSAGE_BUNDLE = "Message Bundle is not available";
 
     private static final String packageName = getPackage(thisClass.getName());
     private static final ClassLoader classLoader = thisClass.getClassLoader();
@@ -36,15 +39,25 @@ public class Messages {
             (MessagesConstants.rootPackageName.equals(packageName))
             ? null
             : MessagesConstants.rootBundle;
+    
+    private static HashMap messageBundleMap = new HashMap ();
 
-
-    /**
-     * ** NO NEED TO CHANGE ANYTHING BELOW ****
-     */
-
-    private static final MessageBundle messageBundle =
+    static {
+    	MessageBundle defaultMessageBundle =
             new MessageBundle(projectName, packageName, resourceName,
                     locale, classLoader, parent);
+    	addMessageBundle(DEFAULT_MESSAGE_BUNDLE_KEY,defaultMessageBundle);
+    }
+    
+    /**
+     * To add a new Message Bundle to the MessageBundle list.
+     * 
+     * @param messageBundleKey The key which will be used to refer to this message bundle later.
+     * @param messageBundle The message bundle.
+     */
+    public static void addMessageBundle (String messageBundleKey,MessageBundle messageBundle) {
+    	messageBundleMap.put(messageBundleKey,messageBundle);
+    }
 
     /**
      * Get a message from resource.properties from the package of the given object.
@@ -54,6 +67,7 @@ public class Messages {
      */
     public static String getMessage(String key)
             throws MissingResourceException {
+    	MessageBundle messageBundle = getMessageBundle (DEFAULT_MESSAGE_BUNDLE_KEY);
         return messageBundle.getMessage(key);
     }
 
@@ -66,6 +80,7 @@ public class Messages {
      */
     public static String getMessage(String key, String arg0)
             throws MissingResourceException {
+    	MessageBundle messageBundle = getMessageBundle (DEFAULT_MESSAGE_BUNDLE_KEY);
         return messageBundle.getMessage(key, arg0);
     }
 
@@ -79,6 +94,7 @@ public class Messages {
      */
     public static String getMessage(String key, String arg0, String arg1)
             throws MissingResourceException {
+    	MessageBundle messageBundle = getMessageBundle (DEFAULT_MESSAGE_BUNDLE_KEY);
         return messageBundle.getMessage(key, arg0, arg1);
     }
 
@@ -93,6 +109,7 @@ public class Messages {
      */
     public static String getMessage(String key, String arg0, String arg1, String arg2)
             throws MissingResourceException {
+    	MessageBundle messageBundle = getMessageBundle (DEFAULT_MESSAGE_BUNDLE_KEY);
         return messageBundle.getMessage(key, arg0, arg1, arg2);
     }
 
@@ -108,6 +125,7 @@ public class Messages {
      */
     public static String getMessage(String key, String arg0, String arg1, String arg2, String arg3)
             throws MissingResourceException {
+    	MessageBundle messageBundle = getMessageBundle (DEFAULT_MESSAGE_BUNDLE_KEY);
         return messageBundle.getMessage(key, arg0, arg1, arg2, arg3);
     }
 
@@ -124,6 +142,7 @@ public class Messages {
      */
     public static String getMessage(String key, String arg0, String arg1, String arg2, String arg3, String arg4)
             throws MissingResourceException {
+    	MessageBundle messageBundle = getMessageBundle (DEFAULT_MESSAGE_BUNDLE_KEY);
         return messageBundle.getMessage(key, arg0, arg1, arg2, arg3, arg4);
     }
 
@@ -136,15 +155,134 @@ public class Messages {
      */
     public static String getMessage(String key, String[] args)
             throws MissingResourceException {
+    	MessageBundle messageBundle = getMessageBundle (DEFAULT_MESSAGE_BUNDLE_KEY);
         return messageBundle.getMessage(key, args);
     }
 
     public static ResourceBundle getResourceBundle() {
+    	MessageBundle messageBundle = getMessageBundle (DEFAULT_MESSAGE_BUNDLE_KEY);
         return messageBundle.getResourceBundle();
     }
 
     public static MessageBundle getMessageBundle() {
+    	MessageBundle messageBundle = getMessageBundle (DEFAULT_MESSAGE_BUNDLE_KEY);
         return messageBundle;
+    }
+    
+    public static MessageBundle getMessageBundle(String messageBundleKey) {
+    	MessageBundle messageBundle = (MessageBundle) messageBundleMap.get (messageBundleKey);
+        return messageBundle;
+    }
+    
+    /**
+     * Get a message from resource.properties from the package of the given object.
+     *
+     * @param messageBundleKey The key for getting the correct message bundle.
+     * @param key The resource key
+     * @return The formatted message
+     */
+    public static String getMessageFromBundle(String messageBundleKey,String key)
+            throws MissingResourceException, Exception {
+    	MessageBundle messageBundle = getMessageBundle (messageBundleKey);
+    	if (messageBundle==null)
+    		throw new Exception (NO_MESSAGE_BUNDLE);
+    	
+        return messageBundle.getMessage(key);
+    }
+
+    /**
+     * Get a message from resource.properties from the package of the given object.
+     *
+     * @param messageBundleKey The key for getting the correct message bundle.
+     * @param key  The resource key
+     * @param arg0 The argument to place in variable {0}
+     * @return The formatted message
+     */
+    public static String getMessageFromBundle(String messageBundleKey,String key, String arg0)
+            throws MissingResourceException, Exception {
+    	MessageBundle messageBundle = getMessageBundle (messageBundleKey);
+    	if (messageBundle==null)
+    		throw new Exception (NO_MESSAGE_BUNDLE);
+    	
+        return messageBundle.getMessage(key, arg0);
+    }
+
+    /**
+     * Get a message from resource.properties from the package of the given object.
+     *
+     * @param messageBundleKey The key for getting the correct message bundle.
+     * @param key  The resource key
+     * @param arg0 The argument to place in variable {0}
+     * @param arg1 The argument to place in variable {1}
+     * @return The formatted message
+     */
+    public static String getMessageFromBundle(String messageBundleKey,String key, String arg0, String arg1)
+            throws MissingResourceException, Exception {
+    	MessageBundle messageBundle = getMessageBundle (messageBundleKey);
+    	if (messageBundle==null)
+    		throw new Exception (NO_MESSAGE_BUNDLE);
+    	
+        return messageBundle.getMessage(key, arg0, arg1);
+    }
+
+    /**
+     * Get a message from resource.properties from the package of the given object.
+     *
+     * @param messageBundleKey The key for getting the correct message bundle.
+     * @param key  The resource key
+     * @param arg0 The argument to place in variable {0}
+     * @param arg1 The argument to place in variable {1}
+     * @param arg2 The argument to place in variable {2}
+     * @return The formatted message
+     */
+    public static String getMessageFromBundle(String messageBundleKey,String key, String arg0, String arg1, String arg2)
+            throws MissingResourceException, Exception {
+    	MessageBundle messageBundle = getMessageBundle (messageBundleKey);
+    	if (messageBundle==null)
+    		throw new Exception (NO_MESSAGE_BUNDLE);
+    	
+        return messageBundle.getMessage(key, arg0, arg1, arg2);
+    }
+
+    /**
+     * Get a message from resource.properties from the package of the given object.
+     *
+     * @param messageBundleKey The key for getting the correct message bundle.
+     * @param key  The resource key
+     * @param arg0 The argument to place in variable {0}
+     * @param arg1 The argument to place in variable {1}
+     * @param arg2 The argument to place in variable {2}
+     * @param arg3 The argument to place in variable {3}
+     * @return The formatted message
+     */
+    public static String getMessageFromBundle(String messageBundleKey,String key, String arg0, String arg1, String arg2, String arg3)
+            throws MissingResourceException, Exception {
+    	MessageBundle messageBundle = getMessageBundle (messageBundleKey);
+    	if (messageBundle==null)
+    		throw new Exception (NO_MESSAGE_BUNDLE);
+    	
+        return messageBundle.getMessage(key, arg0, arg1, arg2, arg3);
+    }
+
+    /**
+     * Get a message from resource.properties from the package of the given object.
+     *
+     * @param messageBundleKey The key for getting the correct message bundle.
+     * @param key  The resource key
+     * @param arg0 The argument to place in variable {0}
+     * @param arg1 The argument to place in variable {1}
+     * @param arg2 The argument to place in variable {2}
+     * @param arg3 The argument to place in variable {3}
+     * @param arg4 The argument to place in variable {4}
+     * @return The formatted message
+     */
+    public static String getMessageFromBundle(String messageBundleKey,String key, String arg0, String arg1, String arg2, String arg3, String arg4)
+            throws MissingResourceException, Exception {
+    	MessageBundle messageBundle = getMessageBundle (messageBundleKey);
+    	if (messageBundle==null)
+    		throw new Exception (NO_MESSAGE_BUNDLE);
+    	
+        return messageBundle.getMessage(key, arg0, arg1, arg2, arg3, arg4);
     }
 
     private static String getPackage(String name) {
