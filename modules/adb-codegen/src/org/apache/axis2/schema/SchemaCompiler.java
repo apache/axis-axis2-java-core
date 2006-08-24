1a81ae3a73c2dd79ec99c253422863129810cf03
Davanum Srinivas <dims@apache.org>
2006-08-24 00:12:22 +0000

    One more fix for (AXIS2-1065) ADB / MTOM databinding is broken

--- modules/adb-codegen/src/org/apache/axis2/schema/SchemaCompiler.java
+++ modules/adb-codegen/src/org/apache/axis2/schema/SchemaCompiler.java
@@ -511,8 +511,16 @@ public class SchemaCompiler {
                     if (schemaType instanceof XmlSchemaComplexType){
                         //set a name
                         schemaType.setName(generatedTypeName.getLocalPart());
+                        // Must do this up front to support recursive types
+                        String fullyQualifiedClassName = writer.makeFullyQualifiedClassName(schemaType.getQName());
+                        processedTypemap.put(schemaType.getQName(), fullyQualifiedClassName);
+                        
+                        BeanWriterMetaInfoHolder metaInfHolder = (BeanWriterMetaInfoHolder) processedAnonymousComplexTypesMap.get(xsElt);
+                        metaInfHolder.setOwnQname(schemaType.getQName());
+                        metaInfHolder.setOwnClassName(fullyQualifiedClassName);
+                        
                         writeComplexType((XmlSchemaComplexType)schemaType,
-                                (BeanWriterMetaInfoHolder)processedAnonymousComplexTypesMap.get(xsElt));
+                                metaInfHolder);
                         //remove the reference from the anon list since we named the type
                         processedAnonymousComplexTypesMap.remove(xsElt);
                         String className = findClassName(schemaType.getQName(), isArray(xsElt));
