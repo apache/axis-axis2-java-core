8a2c5abba005b0eb6c2ac3f7236fc29c877e438a
Davanum Srinivas <dims@apache.org>
2006-09-22 13:29:08 +0000

    prevent NPE

--- modules/kernel/src/org/apache/axis2/description/WSDL11ToAxisServiceBuilder.java
+++ modules/kernel/src/org/apache/axis2/description/WSDL11ToAxisServiceBuilder.java
@@ -674,7 +674,7 @@ public class WSDL11ToAxisServiceBuilder extends WSDLToAxisServiceBuilder {
                                 (QName) resolvedRpcWrappedElementMap.get(
                                         wsdl4jOperation.getName() + WRAPPED_OUTPUTNAME_SUFFIX));
 
-                    } else {
+                    } else if(wsdl4jMessage != null){
                         //pick the first part from the list and take that as the relevant part
                         //it is somewhat questionable whether the first part gets picked
                         //but we'll have to take a chance here
