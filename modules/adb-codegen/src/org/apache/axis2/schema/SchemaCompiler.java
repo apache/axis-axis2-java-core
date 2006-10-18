645aadfdaaad3690e26061be0b0cfadc97747030
Davanum Srinivas <dims@apache.org>
2006-10-18 07:19:50 +0000

    Fix for AXIS2-1271 - Decimal data type - WSDL2JAVA generated bad code (won't compile)

--- modules/adb-codegen/src/org/apache/axis2/schema/SchemaCompiler.java
+++ modules/adb-codegen/src/org/apache/axis2/schema/SchemaCompiler.java
@@ -79,7 +79,7 @@ import java.util.Properties;
 public class SchemaCompiler {
 
     private static final Log log = LogFactory.getLog(SchemaCompiler .class);
-  
+
     private CompilerOptions options;
     private HashMap processedTypemap;
 
@@ -582,6 +582,31 @@ public class SchemaCompiler {
                         xsElt.addMetaInfo(
                                 SchemaConstants.SchemaCompilerInfoHolder.CLASSNAME_KEY,
                                 className);
+                    } else if (schemaType instanceof XmlSchemaSimpleType){
+                        //set a name
+                        schemaType.setName(generatedTypeName.getLocalPart());
+                        // Must do this up front to support recursive types
+                        String fullyQualifiedClassName = writer.makeFullyQualifiedClassName(schemaType.getQName());
+                        processedTypemap.put(schemaType.getQName(), fullyQualifiedClassName);
+
+                        BeanWriterMetaInfoHolder metaInfHolder = (BeanWriterMetaInfoHolder) processedAnonymousComplexTypesMap.get(xsElt);
+                        metaInfHolder.setOwnQname(schemaType.getQName());
+                        metaInfHolder.setOwnClassName(fullyQualifiedClassName);
+
+                        writeSimpleType((XmlSchemaSimpleType)schemaType,
+                                metaInfHolder);
+                        //remove the reference from the anon list since we named the type
+                        processedAnonymousComplexTypesMap.remove(xsElt);
+                        String className = findClassName(schemaType.getQName(), isArray(xsElt));
+                        innerElementMap.put(
+                                xsElt.getQName(),
+                                className);
+
+                        //store in the schema map
+                        xsElt.addMetaInfo(
+                                SchemaConstants.SchemaCompilerInfoHolder.CLASSNAME_KEY,
+                                className);
+
                     }
                 }
             }else{
