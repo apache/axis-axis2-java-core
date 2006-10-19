76d4b7d146d96dbb1f9323e39bf1eb352c816d98
Eran Chinthaka <chinthaka@apache.org>
2006-10-19 05:16:43 +0000

    Completing axis2.sh. Need to test this properly once packaging script is done.

--- modules/tool/script/axis2.sh
+++ modules/tool/script/axis2.sh
@@ -1,24 +1,92 @@
 #!/bin/sh
 
-if [ $AXIS2_HOME="" ]; then
-    export AXIS2_HOME=..
+# ----------------------------------------------------------------------------
+# Axis2 Script
+#
+# Environment Variable Prequisites
+#
+#   AXIS2_HOME   Home of Axis2 installation. If not set I will  try
+#                   to figure it out.
+#
+#   JAVA_HOME       Must point at your Java Development Kit installation.
+#
+# NOTE: Borrowed generously from Apache Tomcat startup scripts.
+# -----------------------------------------------------------------------------
+
+# if JAVA_HOME is not set we're not happy
+if [ -z "$JAVA_HOME" ]; then
+  echo "You must set the JAVA_HOME variable before running Axis2 Script."
+  exit 1
 fi
 
-# let's see we are pointing to the proper Axis2 home
-if [ -e "${AXIS2_HOME}/bin/wsdl2java.sh" ]; then
- echo "Found the proper Axis2 Home"
+# OS specific support.  $var _must_ be set to either true or false.
+cygwin=false
+os400=false
+case "`uname`" in
+CYGWIN*) cygwin=true;;
+OS400*) os400=true;;
+esac
 
-else
-    echo "I can not continue without getting an Axis2 Home"
-fi
+# resolve links - $0 may be a softlink
+PRG="$0"
 
-for f in $AXIS2_HOME/lib/*.jar
-do
-  AXIS2_CLASSPATH=$AXIS2_CLASSPATH:$f
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
 done
-export AXIS2_CLASSPATH
 
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
 
+# Only set AXIS2_HOME if not already set
+[ -z "$AXIS2_HOME" ] && AXIS2_HOME=`cd "$PRGDIR/.." ; pwd`
 
-echo the classpath $AXIS2_CLASSPATH
-java -classpath $AXIS2_CLASSPATH org.apache.axis2.transport.http.SimpleHTTPServer $*
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if $cygwin; then
+  [ -n "$JAVA_HOME" ] && JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+  [ -n "$AXIS2_HOME" ] && AXIS2_HOME=`cygpath --unix "$AXIS2_HOME"`
+  [ -n "$CLASSPATH" ] && CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+fi
+
+# For OS400
+if $os400; then
+  # Set job priority to standard for interactive (interactive - 6) by using
+  # the interactive priority - 6, the helper threads that respond to requests
+  # will be running at the same priority as interactive jobs.
+  COMMAND='chgjob job('$JOBNAME') runpty(6)'
+  system $COMMAND
+
+  # Enable multi threading
+  export QIBM_MULTI_THREADED=Y
+fi
+
+# update classpath
+AXIS2_CLASSPATH=""
+for f in "$AXIS2_HOME"/lib/*.jar
+do
+  AXIS2_CLASSPATH="$AXIS2_CLASSPATH":$f
+done
+AXIS2_CLASSPATH="$JAVA_HOME/lib/tools.jar":"$AXIS2_CLASSPATH":"$CLASSPATH"
+
+# For Cygwin, switch paths to Windows format before running java
+if $cygwin; then
+  JAVA_HOME=`cygpath --absolute --windows "$JAVA_HOME"`
+  AXIS2_HOME=`cygpath --absolute --windows "$AXIS2_HOME"`
+  CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+  JAVA_ENDORSED_DIRS=`cygpath --path --windows "$JAVA_ENDORSED_DIRS"`
+fi
+
+# ----- Execute The Requested Command -----------------------------------------
+
+cd "$AXIS2_HOME"
+echo " Using AXIS2_HOME:   $AXIS2_HOME"
+echo " Using JAVA_HOME:       $JAVA_HOME"
+
+$JAVA_HOME/bin/java -classpath "$AXIS2_CLASSPATH" \
+-Daxis2.xml="$AXIS2_HOME/conf/axis2.xml" -Daxis2.repo="$AXIS2_HOME/repository" $*
+
