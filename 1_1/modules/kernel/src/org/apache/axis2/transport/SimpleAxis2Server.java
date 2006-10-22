723290a4bad8c0780290be09ba6b491aece2e505
Ruchith Udayanga Fernando <ruchithf@apache.org>
2006-10-22 03:12:53 +0000

    Added some missing licence headers with the help of arat ;-) - http://code.google.com/p/arat/

--- 1_1/modules/kernel/src/org/apache/axis2/transport/SimpleAxis2Server.java
+++ 1_1/modules/kernel/src/org/apache/axis2/transport/SimpleAxis2Server.java
@@ -1,26 +1,34 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.axis2.transport;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.xml.namespace.QName;
-
-import org.apache.axis2.Constants;
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ConfigurationContextFactory;
-import org.apache.axis2.description.TransportInDescription;
 import org.apache.axis2.engine.ListenerManager;
 import org.apache.axis2.transport.http.SimpleHTTPServer;
 import org.apache.axis2.util.CommandLineOption;
 import org.apache.axis2.util.CommandLineOptionParser;
-import org.apache.axis2.util.OptionsParser;
 import org.apache.axis2.util.OptionsValidator;
-import org.apache.axis2.util.CommandLineOptionConstants.Java2WSDLConstants;
-import org.apache.axis2.util.CommandLineOptionConstants.WSDL2JavaConstants;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 public class SimpleAxis2Server {
 
--- 1_1/modules/savan/test/org/apache/axis2/savan/EventingSubscripitonProcessorTest.java
+++ 1_1/modules/savan/test/org/apache/axis2/savan/EventingSubscripitonProcessorTest.java
@@ -1,12 +1,20 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.axis2.savan;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-
-import javax.xml.namespace.QName;
-
-import junit.framework.TestCase;
 
 import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axis2.addressing.EndpointReference;
@@ -22,12 +30,19 @@ import org.apache.savan.configuration.ConfigurationManager;
 import org.apache.savan.configuration.Protocol;
 import org.apache.savan.eventing.EventingConstants;
 import org.apache.savan.eventing.EventingSubscriptionProcessor;
-import org.apache.savan.eventing.EventingUtilFactory;
 import org.apache.savan.eventing.subscribers.EventingLeafSubscriber;
 import org.apache.savan.storage.DefaultSubscriberStore;
 import org.apache.savan.storage.SubscriberStore;
 import org.apache.savan.subscription.ExpirationBean;
 import org.apache.savan.util.CommonUtil;
+
+import javax.xml.namespace.QName;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import junit.framework.TestCase;
 
 public class EventingSubscripitonProcessorTest extends TestCase {
 
