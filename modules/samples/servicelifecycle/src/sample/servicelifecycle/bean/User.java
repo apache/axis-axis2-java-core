07dc4c56636f6584f493fd867c12629f35942ec6
Deepal Jayasinghe <deepal@apache.org>
2006-10-27 13:33:42 +0000

    adding default constructor

--- modules/samples/servicelifecycle/src/sample/servicelifecycle/bean/User.java
+++ modules/samples/servicelifecycle/src/sample/servicelifecycle/bean/User.java
@@ -21,6 +21,9 @@ public class User {
     private String userName;
     private String passWord;
 
+    public User() {
+    }
+
     public User(String userName, String passWord) {
         this.userName = userName;
         this.passWord = passWord;
