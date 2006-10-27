5db7fd6948169ffd17dc139fa957a0a45643bd8b
Hettige Don Ignatious Nywan Sanka Samaranayake <sanka@apache.org>
2006-10-27 04:29:20 +0000

    Added the new POJO sample

---
+++ 1_1/modules/samples/pojo/src/address_book_service/AddressBookService.java
@@ -0,0 +1,30 @@
+package address_book_service;
+
+
+import java.util.HashMap;
+
+import address_book_entry.Entry;
+
+
+public class AddressBookService {
+    
+    private HashMap entries = new HashMap();
+
+    /**
+     * Add an Entry to the Address Book
+     * @param entry
+     */
+    public void addEntry(Entry entry) {
+        this.entries.put(entry.getName(), entry);
+    }
+    
+    /**
+     * Search an address of a person
+     * 
+     * @param name the name of the person whose address needs to be found
+     * @return return the address entry of the person. 
+     */
+    public Entry findEntry(String name) {
+        return (Entry) this.entries.get(name);
+    }
+}
