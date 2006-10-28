f93f1e6a0407c1278a0091d6f48014a70ee61091
Davanum Srinivas <dims@apache.org>
2006-10-28 03:05:06 +0000

    Fix for AXIS2-1533 - FaultCode is not set in the AxisFault object

--- 1_1/modules/adb/src/org/apache/axis2/rpc/receivers/RPCMessageReceiver.java
+++ 1_1/modules/adb/src/org/apache/axis2/rpc/receivers/RPCMessageReceiver.java
@@ -139,14 +139,18 @@ public class RPCMessageReceiver extends AbstractInOutSyncMessageReceiver {
             outMessage.setEnvelope(envelope);
         } catch (InvocationTargetException e) {
             String msg = null;
-            if (e.getCause() != null) {
-                msg = e.getCause().getMessage();
+            Throwable cause = e.getCause();
+            if (cause != null) {
+                msg = cause.getMessage();
             }
             if (msg == null) {
                 msg = "Exception occurred while trying to invoke service method " +
                         method.getName();
             }
             log.error(msg, e);
+            if(cause instanceof AxisFault) {
+                throw (AxisFault)cause;
+            }
             throw new AxisFault(msg);
         } catch (Exception e) {
             String msg = "Exception occurred while trying to invoke service method " +
