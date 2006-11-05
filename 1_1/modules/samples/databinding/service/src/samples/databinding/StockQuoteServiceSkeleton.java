818dc9ab6e6dce6058ab968f0c8bce5377f58aab
Davanum Srinivas <dims@apache.org>
2006-11-05 02:57:53 +0000

    - Added a JDOM Based client that illustrates the OMDataSource - Excluded from build because it needs 3 files for bridging stax/jdom from   https://svn.codehaus.org/woodstox/utils/jdom/ - Clean up imports

--- 1_1/modules/samples/databinding/service/src/samples/databinding/StockQuoteServiceSkeleton.java
+++ 1_1/modules/samples/databinding/service/src/samples/databinding/StockQuoteServiceSkeleton.java
@@ -5,23 +5,23 @@
  * by the Apache Axis2 version: 1.1-SNAPSHOT Nov 03, 2006 (06:54:07 EST)
  */
 package samples.databinding;
+import javanet.staxutils.StAXSource;
 import org.apache.axiom.om.impl.builder.SAXOMBuilder;
 import org.exolab.castor.xml.MarshalException;
 import org.exolab.castor.xml.Marshaller;
+import org.exolab.castor.xml.UnmarshalHandler;
 import org.exolab.castor.xml.Unmarshaller;
 import org.exolab.castor.xml.ValidationException;
-import org.exolab.castor.xml.UnmarshalHandler;
-import org.xml.sax.SAXException;
 import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import samples.databinding.data.Change;
 import samples.databinding.data.GetStockQuote;
 import samples.databinding.data.GetStockQuoteResponse;
 import samples.databinding.data.LastTrade;
 import samples.databinding.data.Quote;
-import samples.databinding.data.Change;
 
-import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXResult;
 import java.io.IOException;
 import java.util.Calendar;
@@ -35,8 +35,8 @@ public class StockQuoteServiceSkeleton {
      * @param param0
      */
     public org.apache.axiom.om.OMElement getStockQuote(org.apache.axiom.om.OMElement param0) {
-        javanet.staxutils.StAXSource staxSource =
-                new javanet.staxutils.StAXSource(param0.getXMLStreamReader());
+        StAXSource staxSource =
+                new StAXSource(param0.getXMLStreamReader());
         Unmarshaller unmarshaller = new Unmarshaller(GetStockQuote.class);
         UnmarshalHandler unmarshalHandler = unmarshaller.createHandler();
         GetStockQuote stockQuote;
@@ -55,7 +55,7 @@ public class StockQuoteServiceSkeleton {
         quote.setVolume(5000);
 
         LastTrade lastTrade = new LastTrade();
-        lastTrade.setPrice(99.99);
+        lastTrade.setPrice(99);
         lastTrade.setDate(Calendar.getInstance().getTimeInMillis());
         quote.setLastTrade(lastTrade);
 
