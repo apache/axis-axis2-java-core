f7c3a0beed8759b46c4f44a7d32c39bd29f4a475
David Illsley <davidillsley@apache.org>
2006-12-08 13:16:12 +0000

    First part of AXIS2-1457 - New simple dispatchers which dispatch either service or operation - Unit tests for the new classes - Will hook these into axis2.xml in future

---
+++ modules/kernel/src/org/apache/axis2/dispatchers/RelatesToBasedServiceDispatcher.java
@@ -0,0 +1,54 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.apache.axis2.dispatchers;
+
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.addressing.RelatesTo;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.context.OperationContext;
+import org.apache.axis2.description.AxisService;
+import org.apache.axis2.description.HandlerDescription;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class RelatesToBasedServiceDispatcher extends AbstractServiceDispatcher {
+
+    public static final String NAME = "RelatesToBasedServiceDispatcher";
+    private static final Log log = LogFactory.getLog(RelatesToBasedServiceDispatcher.class);
+    private static final boolean isDebugEnabled = log.isDebugEnabled();
+
+    public AxisService findService(MessageContext messageContext) throws AxisFault {
+        RelatesTo relatesTo = messageContext.getRelatesTo();
+        if(isDebugEnabled){
+            log.debug("Checking for OperationContext using RelatesTo : " + relatesTo);
+        }
+        if((relatesTo!=null) && (relatesTo.getValue()!=null)){
+            ConfigurationContext configurationContext = messageContext.getConfigurationContext();
+            OperationContext operationContext = configurationContext.getOperationContext(relatesTo.getValue());
+            if(operationContext != null){
+                if(isDebugEnabled){
+                    log.debug("Found OperationContext: " + operationContext);
+                }
+                return operationContext.getServiceContext().getAxisService();
+            }
+        }
+        return null;
+    }
+    
+    public void initDispatcher() {
+        init(new HandlerDescription(NAME));
+    }
+}
