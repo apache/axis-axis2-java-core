9f76f84d537cd94805e7e44a03033ae2b557c27c
Davanum Srinivas <dims@apache.org>
2007-01-05 02:26:32 +0000

    buffer the outputstream as well

--- 1_1/modules/kernel/src/org/apache/axis2/transport/http/AxisServlet.java
+++ 1_1/modules/kernel/src/org/apache/axis2/transport/http/AxisServlet.java
@@ -53,6 +53,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.net.SocketException;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -250,7 +251,7 @@ public class AxisServlet extends HttpServlet implements TransportListener {
 
             try {
                 // adding ServletContext into msgContext;
-                HTTPTransportUtils.processHTTPPostRequest(msgContext, new BufferedInputStream(req.getInputStream()), out,
+                HTTPTransportUtils.processHTTPPostRequest(msgContext, new BufferedInputStream(req.getInputStream()), new BufferedOutputStream(out),
                         contentType, req.getHeader(HTTPConstants.HEADER_SOAP_ACTION),
                         req.getRequestURL().toString());
 
