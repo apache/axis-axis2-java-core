1786a427e58d2c5538526d2cc7a1c87a9a412cde
Bill Nagy <nagy@apache.org>
2007-02-24 21:10:10 +0000

    Commented out some tests that have not been committed to fix a build break

--- modules/jaxws/test/org/apache/axis2/jaxws/framework/JAXWSTest.java
+++ modules/jaxws/test/org/apache/axis2/jaxws/framework/JAXWSTest.java
@@ -76,8 +76,8 @@ import org.apache.axis2.jaxws.sample.WrapTests;
 import org.apache.axis2.jaxws.security.BasicAuthSecurityTests;
 import org.apache.axis2.jaxws.xmlhttp.clientTests.dispatch.datasource.DispatchXMessageDataSource;
 import org.apache.axis2.jaxws.xmlhttp.clientTests.dispatch.jaxb.DispatchXPayloadJAXB;
-import org.apache.axis2.jaxws.xmlhttp.clientTests.dispatch.source.DispatchXMessageSource;
-import org.apache.axis2.jaxws.xmlhttp.clientTests.dispatch.source.DispatchXPayloadSource;
+//import org.apache.axis2.jaxws.xmlhttp.clientTests.dispatch.source.DispatchXMessageSource;
+//import org.apache.axis2.jaxws.xmlhttp.clientTests.dispatch.source.DispatchXPayloadSource;
 import org.apache.axis2.jaxws.xmlhttp.clientTests.dispatch.string.DispatchXMessageString;
 import org.apache.axis2.jaxws.xmlhttp.clientTests.dispatch.string.DispatchXPayloadString;
 import org.apache.log4j.BasicConfigurator;
@@ -167,8 +167,8 @@ public class JAXWSTest extends TestCase {
         
         suite.addTestSuite(DispatchXPayloadString.class);
         suite.addTestSuite(DispatchXMessageString.class);
-        suite.addTestSuite(DispatchXPayloadSource.class);
-        suite.addTestSuite(DispatchXMessageSource.class);
+//        suite.addTestSuite(DispatchXPayloadSource.class);
+//        suite.addTestSuite(DispatchXMessageSource.class);
         suite.addTestSuite(DispatchXPayloadJAXB.class);
         suite.addTestSuite(DispatchXMessageDataSource.class);
         
