92f7cef1752e83cbf2d38a66155187bd4d7d78dc
Nanayakkara Kuruppuge Thilina Gunarathne <thilina@apache.org>
2007-02-26 17:35:15 +0000

    removing duplicate code... Adding license headers

--- modules/samples/ping/src/samples/ping/receivers/RawXMLINOnlyMessageReceiver.java
+++ modules/samples/ping/src/samples/ping/receivers/RawXMLINOnlyMessageReceiver.java
@@ -11,55 +11,8 @@ import org.apache.axis2.i18n.Messages;
 
 import java.lang.reflect.Method;
 
-public class RawXMLINOnlyMessageReceiver extends AbstractInMessageReceiver
+public class RawXMLINOnlyMessageReceiver extends org.apache.axis2.receivers.RawXMLINOnlyMessageReceiver
         implements MessageReceiver, Pingable {
-
-    private Method findOperation(AxisOperation op, Class implClass) {
-        String methodName = op.getName().getLocalPart();
-        Method[] methods = implClass.getMethods();
-
-        for (int i = 0; i < methods.length; i++) {
-            if (methods[i].getName().equals(methodName) &&
-                    methods[i].getParameterTypes().length == 1 &&
-                    OMElement.class.getName().equals(
-                            methods[i].getParameterTypes()[0].getName()) &&
-                    "void".equals(methods[i].getReturnType().getName())) {
-                return methods[i];
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Invokes the bussiness logic invocation on the service implementation class
-     *
-     * @param msgContext the incoming message context
-     * @throws org.apache.axis2.AxisFault on invalid method (wrong signature)
-     */
-    public void invokeBusinessLogic(MessageContext msgContext) throws AxisFault {
-        try {
-
-            // get the implementation class for the Web Service
-            Object obj = getTheImplementationObject(msgContext);
-
-            // find the WebService method
-            Class implClass = obj.getClass();
-
-            AxisOperation op = msgContext.getAxisOperation();
-            Method method = findOperation(op, implClass);
-
-            if (method != null) {
-                method.invoke(
-                        obj, new Object[]{msgContext.getEnvelope().getBody().getFirstElement()});
-            } else {
-                throw new AxisFault(Messages.getMessage("methodDoesNotExistInOnly"));
-            }
-
-        } catch (Exception e) {
-            throw AxisFault.makeFault(e);
-        }
-    }
 
     public int ping() throws AxisFault {
         // get the implementation class for the Web Service
--- modules/samples/ping/src/samples/ping/receivers/RawXMLINOutMessageReceiver.java
+++ modules/samples/ping/src/samples/ping/receivers/RawXMLINOutMessageReceiver.java
@@ -1,6 +1,23 @@
+/*
+* Copyright 2004,2005 The Apache Software Foundation.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package samples.ping.receivers;
 
 import org.apache.axis2.receivers.AbstractInOutSyncMessageReceiver;
+import org.apache.axis2.receivers.RawXMLINOnlyMessageReceiver;
 import org.apache.axis2.engine.MessageReceiver;
 import org.apache.axis2.description.AxisOperation;
 import org.apache.axis2.context.MessageContext;
@@ -13,65 +30,8 @@ import org.apache.axis2.engine.Pingable;
 
 import java.lang.reflect.Method;
 
-public class RawXMLINOutMessageReceiver extends AbstractInOutSyncMessageReceiver
+public class RawXMLINOutMessageReceiver extends org.apache.axis2.receivers.RawXMLINOutMessageReceiver
         implements MessageReceiver, Pingable {
-
-    private Method findOperation(AxisOperation op, Class implClass) {
-        String methodName = op.getName().getLocalPart();
-        Method[] methods = implClass.getMethods();
-
-        for (int i = 0; i < methods.length; i++) {
-            if (methods[i].getName().equals(methodName) &&
-                    methods[i].getParameterTypes().length == 1 &&
-                    OMElement.class.getName().equals(
-                            methods[i].getParameterTypes()[0].getName()) &&
-                    OMElement.class.getName().equals(methods[i].getReturnType().getName())) {
-                return methods[i];
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Invokes the bussiness logic invocation on the service implementation class
-     *
-     * @param msgContext    the incoming message context
-     * @param newmsgContext the response message context
-     * @throws org.apache.axis2.AxisFault on invalid method (wrong signature) or behaviour (return null)
-     */
-    public void invokeBusinessLogic(MessageContext msgContext, MessageContext newmsgContext)
-            throws AxisFault {
-        try {
-
-            // get the implementation class for the Web Service
-            Object obj = getTheImplementationObject(msgContext);
-
-            // find the WebService method
-            Class implClass = obj.getClass();
-
-            AxisOperation opDesc = msgContext.getAxisOperation();
-            Method method = findOperation(opDesc, implClass);
-
-            if (method != null) {
-                OMElement result = (OMElement) method.invoke(
-                        obj, new Object[]{msgContext.getEnvelope().getBody().getFirstElement()});
-                SOAPFactory fac = getSOAPFactory(msgContext);
-                SOAPEnvelope envelope = fac.getDefaultEnvelope();
-
-                if (result != null) {
-                    envelope.getBody().addChild(result);
-                }
-
-                newmsgContext.setEnvelope(envelope);
-
-            } else {
-                throw new AxisFault(Messages.getMessage("methodDoesNotExistInOut"));
-            }
-        } catch (Exception e) {
-            throw AxisFault.makeFault(e);
-        }
-    }
 
     public int ping() throws AxisFault{
         // get the implementation class for the Web Service
