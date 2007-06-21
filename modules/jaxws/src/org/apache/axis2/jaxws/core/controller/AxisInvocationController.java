c0c1cc0351558f566d10bea94fa0728157887207
Nicholas L. Gallardo <ngallardo@apache.org>
2007-06-21 20:01:11 +0000

    Set the timeout properly for JAX-WS clients.

--- modules/jaxws/src/org/apache/axis2/jaxws/core/controller/AxisInvocationController.java
+++ modules/jaxws/src/org/apache/axis2/jaxws/core/controller/AxisInvocationController.java
@@ -392,6 +392,13 @@ public class AxisInvocationController extends InvocationController {
             // Get the SOAP Action (if needed)
             String soapAction = ClientUtils.findSOAPAction(requestMsgCtx);
             options.setAction(soapAction);
+            // get the timeout from the request message context options as it may have been
+            // set by the user; if it was not set by the user we will just be setting the
+            // timeout on the operation client to the default so it will not have a negative
+            // effect; this logic is reliant on the fact the JAX-WS MessageContext is delegating
+            // to the Axis2 Options object and not storing its own property bag
+            long timeout = axisRequest.getOptions().getTimeOutInMilliSeconds();
+            options.setTimeOutInMilliSeconds(timeout);
 
             // Use the OperationClient to send the request and put the contents
             // of the response in the response MessageContext.
