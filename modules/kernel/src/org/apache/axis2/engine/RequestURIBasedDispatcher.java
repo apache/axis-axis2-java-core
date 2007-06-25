dbe2c4fcbf97a83769e5a8da02a7a568c6756e5d
Davanum Srinivas <dims@apache.org>
2007-06-25 16:34:45 +0000

    Moving classes to the correct package. add deprecated stubs that extend the correct class for backward compat. This is step 1. Need to fix references in existing axis2.xml's in the next check-in and maybe add a log.warn if old class is used when loading dispatchers

--- modules/kernel/src/org/apache/axis2/engine/RequestURIBasedDispatcher.java
+++ modules/kernel/src/org/apache/axis2/engine/RequestURIBasedDispatcher.java
@@ -13,41 +13,10 @@
 */
 package org.apache.axis2.engine;
 
-import org.apache.axis2.AxisFault;
-import org.apache.axis2.context.MessageContext;
-import org.apache.axis2.description.AxisOperation;
-import org.apache.axis2.description.AxisService;
-import org.apache.axis2.description.HandlerDescription;
-import org.apache.axis2.dispatchers.RequestURIBasedServiceDispatcher;
-
 /**
  * Dispatches the service based on the information from the target endpoint URL.
+ *
+ * @deprecated use org.apache.axis2.dispatchers.RequestURIBasedDispatcher
  */
-public class RequestURIBasedDispatcher extends AbstractDispatcher {
-
-    public static final String NAME = "RequestURIBasedDispatcher";
-    private RequestURIBasedServiceDispatcher rubsd = new RequestURIBasedServiceDispatcher();
-    
-    /*
-     *  (non-Javadoc)
-     * @see org.apache.axis2.engine.AbstractDispatcher#findOperation(org.apache.axis2.description.AxisService, org.apache.axis2.context.MessageContext)
-     */
-    public AxisOperation findOperation(AxisService service, MessageContext messageContext)
-            throws AxisFault {
-        // This Dispatcher does not need to resolve the operation, as that is handled
-        // by the RequestURIOperationDispatcher.
-        return null;
-    }
-
-    /*
-     *  (non-Javadoc)
-     * @see org.apache.axis2.engine.AbstractDispatcher#findService(org.apache.axis2.context.MessageContext)
-     */
-    public AxisService findService(MessageContext messageContext) throws AxisFault {
-    	return rubsd.findService(messageContext);
-    }
-
-    public void initDispatcher() {
-        init(new HandlerDescription(NAME));
-    }
+public class RequestURIBasedDispatcher extends org.apache.axis2.dispatchers.RequestURIBasedDispatcher {
 }
