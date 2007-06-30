d4356673bf6cca8d8cb3e68b9f3e5fa089b5a53e
Davanum Srinivas <dims@apache.org>
2007-06-30 15:09:14 +0000

    Fix build break. update license header too. I need to reopen Axis2-2881

--- modules/codegen/src/org/apache/axis2/wsdl/codegen/emitter/AxisServiceBasedMultiLanguageEmitter.java
+++ modules/codegen/src/org/apache/axis2/wsdl/codegen/emitter/AxisServiceBasedMultiLanguageEmitter.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.axis2.wsdl.codegen.emitter;
 
 import org.apache.axis2.AxisFault;
@@ -73,25 +91,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import com.ibm.wsdl.util.xml.DOM2Writer;
-
-/*
- * Copyright 2004,2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 
 public class AxisServiceBasedMultiLanguageEmitter implements Emitter {
 
@@ -392,11 +391,12 @@ public class AxisServiceBasedMultiLanguageEmitter implements Emitter {
                         containsKey(faultMessage.getName())) {
                     //make a name
                     String className = makeJavaClassName(faultMessage.getName());
-                    QName faultQName = new QName(codeGenConfiguration.getTargetNamespace(), faultMessage.getName());
-                    if (this.mapper.getTypeMappingName(faultQName) != null) {
-                        // i.e we already have an entry
-                        className = makeJavaClassName(className + (uniqueFaultNameCounter++));
-                    }
+//  FIXME: TODO: the following lines were for fixing Axis2-2881 (svn revision 552106), but causes build break.
+//                    QName faultQName = new QName(codeGenConfiguration.getTargetNamespace(), faultMessage.getName());
+//                    if (this.mapper.getTypeMappingName(faultQName) != null) {
+//                        // i.e we already have an entry
+//                        className = makeJavaClassName(className + (uniqueFaultNameCounter++));
+//                    }
                     while (fullyQualifiedFaultClassNameMap.containsValue(className)) {
                         className = makeJavaClassName(className + (uniqueFaultNameCounter++));
                     }
