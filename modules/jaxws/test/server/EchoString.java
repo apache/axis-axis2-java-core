38bdb248eb7dd7b81ed1aca488e2644588562689
Amila Chinthaka Suriarachchi <amilas@apache.org>
2007-11-19 05:41:06 +0000

    add serialization methods to avoid compilation errors

--- modules/jaxws/test/server/EchoString.java
+++ modules/jaxws/test/server/EchoString.java
@@ -26,19 +26,27 @@
             */
 
             package server;
+
+            import org.apache.axis2.databinding.utils.writer.MTOMAwareXMLStreamWriter;
+            import org.apache.axis2.databinding.ADBException;
+            import org.apache.axiom.om.OMFactory;
+
+            import javax.xml.namespace.QName;
+            import javax.xml.stream.XMLStreamException;
+
             /**
             *  EchoString bean class
             */
-        
-        public  class EchoString
+
+            public  class EchoString
         implements org.apache.axis2.databinding.ADBBean{
-        
+
                 public static final javax.xml.namespace.QName MY_QNAME = new javax.xml.namespace.QName(
                 "http://test",
                 "echoString",
                 "ns1");
 
-            
+
 
 
             /**
@@ -46,8 +54,8 @@
             */
 
             protected java.lang.String localInput ;
-           
-           
+
+
 
            /**
            * Auto generated getter method
@@ -59,17 +67,17 @@
                return localInput;
            }
 
-           
-            
+
+
                     /**
                    * Auto generated setter method
                    * @param param Input
                    */
                    public void setInput(java.lang.String param){
-                    
+
                    this.localInput=param;
                    }
-                
+
 
         /**
         * databinding method to get an XML representation of this object
@@ -78,33 +86,48 @@
         public javax.xml.stream.XMLStreamReader getPullParser(javax.xml.namespace.QName qName){
 
 
-        
+
                  java.util.ArrayList elementList = new java.util.ArrayList();
                  java.util.ArrayList attribList = new java.util.ArrayList();
 
-                
+
                              elementList.add(new javax.xml.namespace.QName("http://test",
                                                                       "input"));
-                            
+
                                     elementList.add(localInput==null?null:
                                      org.apache.axis2.databinding.utils.ConverterUtil.convertToString(localInput));
-                                
+
 
                 return new org.apache.axis2.databinding.utils.reader.ADBXMLStreamReaderImpl(qName, elementList.toArray(), attribList.toArray());
-            
-            
+
+
 
         }
 
+                public void serialize(final QName parentQName,
+                                      final OMFactory factory,
+                                      MTOMAwareXMLStreamWriter xmlWriter)
+                        throws XMLStreamException, ADBException {
+                    serialize(parentQName, factory, xmlWriter, false);
+                }
+
+                public void serialize(final QName parentQName,
+                                      final OMFactory factory,
+                                      MTOMAwareXMLStreamWriter xmlWriter,
+                                      boolean serializeType)
+                        throws XMLStreamException, ADBException {
+                    throw new UnsupportedOperationException("Un implemented method");
+                }
 
 
 
-    
+
+
 
         /**
              *  Factory class that keeps the parse method
              */
-           public static class Factory{
+        public static class Factory{
 
                /**
                * static method to create the object
@@ -119,11 +142,11 @@
                    event = reader.next();
                }
 
-               
+
                if (!MY_QNAME.equals(reader.getName())){
                            throw new Exception("Wrong QName");
                }
-               
+
                        org.apache.axis2.databinding.utils.SimpleElementReaderStateMachine stateMachine1
                          = new org.apache.axis2.databinding.utils.SimpleElementReaderStateMachine();
                        javax.xml.namespace.QName startQname1 = new javax.xml.namespace.QName(
@@ -136,7 +159,7 @@
                          stateMachine1.getText()==null?null:
                                     org.apache.axis2.databinding.utils.ConverterUtil.convertToString(
                                   stateMachine1.getText()));
-                             
+
                } catch (javax.xml.stream.XMLStreamException e) {
                     throw new java.lang.Exception(e);
                }
@@ -146,9 +169,9 @@
                }//end of factory class
 
 
-     
 
-        
+
+
         }
            
           
