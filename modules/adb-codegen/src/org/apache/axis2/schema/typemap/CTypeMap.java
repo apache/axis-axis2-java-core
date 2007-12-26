38ded8c471923bfbc008cdac0a2947350c6d8eef
Dimuthu Gamage <dimuthu@apache.org>
2007-12-26 15:30:47 +0000

    Quick changes just to make sure WSDL2C works with ADB with new changes.

--- modules/adb-codegen/src/org/apache/axis2/schema/typemap/CTypeMap.java
+++ modules/adb-codegen/src/org/apache/axis2/schema/typemap/CTypeMap.java
@@ -19,7 +19,12 @@
 package org.apache.axis2.schema.typemap;
 
 import org.apache.axis2.wsdl.codegen.emitter.CTypeInfo;
+import org.apache.axis2.schema.SchemaConstants;
+import java.util.Map;
 
+import javax.xml.namespace.QName;
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -34,7 +39,54 @@ public class CTypeMap implements TypeMap{
     }
 
     public Map getSoapEncodingTypesMap() {
-        return null;
+        return soapEncodingTypeMap;
     }
 
+    private static Map soapEncodingTypeMap = new HashMap();
+
+    static {
+        // populate the soapEncodingTypeMap
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_ARRAY,
+                org.apache.axis2.databinding.types.soapencoding.Array.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_STRUCT,
+                org.apache.axis2.databinding.types.soapencoding.Struct.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_BASE64,
+                org.apache.axis2.databinding.types.soapencoding.Base64.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_DURATION,
+                org.apache.axis2.databinding.types.soapencoding.Duration.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_DATETIME,
+                org.apache.axis2.databinding.types.soapencoding.DateTime.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_NOTATION,
+                org.apache.axis2.databinding.types.soapencoding.NOTATION.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_TIME,
+                org.apache.axis2.databinding.types.soapencoding.Time.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_DATE,
+                org.apache.axis2.databinding.types.soapencoding.Date.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_GYEARMONTH,
+                org.apache.axis2.databinding.types.soapencoding.GYearMonth.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_GYEAR,
+                org.apache.axis2.databinding.types.soapencoding.GYear.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_GMONTHDAY,
+                org.apache.axis2.databinding.types.soapencoding.GMonthDay.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_GDAY,
+                org.apache.axis2.databinding.types.soapencoding.GDay.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_GMONTH,
+                org.apache.axis2.databinding.types.soapencoding.GMonth.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_BOOLEAN,
+                org.apache.axis2.databinding.types.soapencoding._boolean.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_BASE64BINARY,
+                org.apache.axis2.databinding.types.soapencoding.Base64Binary.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_HEXBINARY,
+                org.apache.axis2.databinding.types.soapencoding.HexBinary.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_FLOAT,
+                org.apache.axis2.databinding.types.soapencoding._float.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_DOUBLE,
+                org.apache.axis2.databinding.types.soapencoding._double.class.getName());
+        addSoapEncodingTypeMapping(SchemaConstants.SOAP_ENCODING_ANYURI,
+                org.apache.axis2.databinding.types.soapencoding.AnyURI.class.getName());
+    }
+
+    private static void addSoapEncodingTypeMapping(QName name, String className) {
+        soapEncodingTypeMap.put(name, className);
+    }
 }
