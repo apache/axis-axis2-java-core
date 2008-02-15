84ff9765805047c8fefbd421a4e351e8a09654a6
Davanum Srinivas <dims@apache.org>
2008-02-15 14:43:43 +0000

    Experimenting with OSGi

---
+++ modules/osgi/src/org/apache/axis2/extensions/osgi/Activator.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.axis2.extensions.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+
+import javax.servlet.Servlet;
+
+public class Activator implements BundleActivator {
+
+    BundleContext context;
+
+    public void start(BundleContext context) throws Exception {
+        this.context = context;
+        doServletRegistration();
+    }
+
+
+    private void doServletRegistration() {
+        ServiceReference sr = context.getServiceReference(HttpService.class.getName());
+        if (sr != null) {
+            HttpService httpServ = (HttpService) context.getService(sr);
+
+            try {
+                Servlet servlet = new OSGiAxis2Servlet();
+                httpServ.registerServlet("/axis2",
+                        servlet, null, null);
+            } catch (Exception e) {
+                System.err.println("Exception registering Axis Servlet:"
+                        + e);
+            }
+        }
+    }
+
+    public void stop(BundleContext context) throws Exception {
+    }
+}
---
+++ modules/osgi/src/org/apache/axis2/extensions/osgi/OSGiAxis2Servlet.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.axis2.extensions.osgi;
+
+import org.apache.axis2.transport.http.AxisServlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+public class OSGiAxis2Servlet extends AxisServlet {
+    public void init(ServletConfig config) throws ServletException {
+        // Fix for a class loader issue in Felix
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+        try {
+            super.init(config);
+        } finally {
+            Thread.currentThread().setContextClassLoader(cl);
+        }
+    }
+
+    /**
+     * Set the context root if it is not set already. We are overriding AxisServlet's impl to
+     * get past an issue in Felix.
+     *
+     * @param req
+     */
+    public void initContextRoot(HttpServletRequest req) {
+        if (contextRoot != null && contextRoot.trim().length() != 0) {
+            return;
+        }
+        String contextPath = req.getServletPath();
+
+        //handling ROOT scenario, for servlets in the default (root) context, this method returns ""
+        if (contextPath != null && contextPath.length() == 0) {
+            contextPath = "/";
+        }
+        this.contextRoot = contextPath;
+
+        configContext.setContextRoot(contextRoot);
+    }
+}
