7b9b4032ebad9ecc6740c55979ee85b2c559a58c
Roy Wood <woodroy@apache.org>
2008-03-03 18:47:38 +0000

    JIRA: AXIS2-3550 Contributor: Roy Wood Description: Improve structure of JAXWS end-to-end tests Initial Drop to split out the JAXWS client-server tests to 'jaxws-integration' and leave unit tests in jaxws/test

---
+++ modules/jaxws-integration/test/org/apache/axis2/jaxws/client/dispatch/InvocationControllerTest.java
@@ -0,0 +1,78 @@
+package org.apache.axis2.jaxws.client.dispatch;
+
+import org.apache.axis2.jaxws.core.InvocationContext;
+import org.apache.axis2.jaxws.core.controller.InvocationController;
+import org.apache.axis2.jaxws.core.controller.InvocationControllerFactory;
+import org.apache.axis2.jaxws.core.controller.impl.AxisInvocationController;
+import org.apache.axis2.jaxws.core.controller.impl.InvocationControllerFactoryImpl;
+import org.apache.axis2.jaxws.registry.FactoryRegistry;
+
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+import javax.xml.ws.AsyncHandler;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Response;
+import javax.xml.ws.Service;
+import javax.xml.ws.soap.SOAPBinding;
+
+import java.util.concurrent.Future;
+
+import junit.framework.TestCase;
+
+public class InvocationControllerTest extends TestCase {
+
+    private QName svcQname = new QName("http://test", "TestService");
+    private QName portQname = new QName("http://test", "TestPort");
+    
+    public void testDefaultInvocationController() {
+        Service svc = Service.create(svcQname);
+        svc.addPort(portQname, SOAPBinding.SOAP11HTTP_BINDING, "http://localhost");
+        Dispatch d = svc.createDispatch(portQname, Source.class, Service.Mode.PAYLOAD);
+        
+        BaseDispatch bd = (BaseDispatch) d;
+        
+        assertTrue("An InvocationController instance was not created", bd.ic != null);
+        assertTrue("The default InvocationController type was incorrect.", 
+            AxisInvocationController.class.isAssignableFrom(bd.ic.getClass()));
+    }
+    
+    public void testPluggableInvocationController() {
+        FactoryRegistry.setFactory(InvocationControllerFactory.class, new TestInvocationControllerFactory());
+        
+        Service svc = Service.create(svcQname);
+        svc.addPort(portQname, SOAPBinding.SOAP11HTTP_BINDING, "http://localhost");
+        Dispatch d = svc.createDispatch(portQname, Source.class, Service.Mode.PAYLOAD);
+        
+        BaseDispatch bd = (BaseDispatch) d;
+        
+        // Set it back to the default so we don't break other tests.
+        FactoryRegistry.setFactory(InvocationControllerFactory.class, new InvocationControllerFactoryImpl());
+        
+        assertTrue("An InvocationController instance was not created", bd.ic != null);
+        assertTrue("The default InvocationController type was incorrect.", 
+            TestInvocationController.class.isAssignableFrom(bd.ic.getClass()));
+    }
+}
+
+class TestInvocationControllerFactory implements InvocationControllerFactory {
+    public InvocationController getInvocationController() {
+        return new TestInvocationController(); 
+    }
+}
+
+class TestInvocationController implements InvocationController {
+
+    public InvocationContext invoke(InvocationContext ic) {
+        return null;
+    }
+
+    public Future<?> invokeAsync(InvocationContext ic, AsyncHandler asyncHandler) {
+        return null;
+    }
+
+    public Response invokeAsync(InvocationContext ic) {
+        return null;
+    }
+
+    public void invokeOneWay(InvocationContext ic) throws Exception {}    
+}
