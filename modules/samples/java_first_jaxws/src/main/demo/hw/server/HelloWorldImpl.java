0e7a78368c8092652078a85cb0a2011d1d0513a2
Davanum Srinivas <dims@apache.org>
2008-03-18 13:33:52 +0000

    update license headers

--- modules/samples/java_first_jaxws/src/main/demo/hw/server/HelloWorldImpl.java
+++ modules/samples/java_first_jaxws/src/main/demo/hw/server/HelloWorldImpl.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-// START SNIPPET: service
 package demo.hw.server;
 
 import java.util.LinkedHashMap;
@@ -31,4 +30,3 @@ public class HelloWorldImpl implements HelloWorld {
         return "Hello " + text;
     }
 }
-// END SNIPPET: service
