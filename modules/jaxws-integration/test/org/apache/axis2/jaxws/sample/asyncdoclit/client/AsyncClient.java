34079d52208ab9bd913e0e65b3455cd956647c2e
Michael Rheinheimer <rott@apache.org>
2008-03-26 21:37:54 +0000

    fix license headers on previous file commits.  Oops.

--- modules/jaxws-integration/test/org/apache/axis2/jaxws/sample/asyncdoclit/client/AsyncClient.java
+++ modules/jaxws-integration/test/org/apache/axis2/jaxws/sample/asyncdoclit/client/AsyncClient.java
@@ -1,17 +1,21 @@
-//
-// @(#) 1.3 FVT/ws/code/websvcs.fvt/src/jaxws/async/wsfvt/test/AsyncClient.java, WAS.websvcs.fvt, WASX.FVT 5/15/07 09:07:54 [7/11/07 13:14:36]
-//
-// IBM Confidential OCO Source Material
-// (C) COPYRIGHT International Business Machines Corp. 2006
-// The source code for this program is not published or otherwise divested
-// of its trade secrets, irrespective of what has been deposited with the
-// U.S. Copyright Office.
-//
-// Change History:
-// Date     UserId      Defect           Description
-// ----------------------------------------------------------------------------
-// 05/11/07 sedov       438719           Added more trace
-// 05/15/07 sedov       439294           Added timer to isAsleepCheck
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.axis2.jaxws.sample.asyncdoclit.client;
 
 import java.util.Map;
--- modules/jaxws-integration/test/org/apache/axis2/jaxws/sample/asyncdoclit/server/DocLitWrappedPortImpl.java
+++ modules/jaxws-integration/test/org/apache/axis2/jaxws/sample/asyncdoclit/server/DocLitWrappedPortImpl.java
@@ -1,17 +1,21 @@
-//
-// @(#) 1.2 WFVT/ws/code/websvcs.fvt/src/jaxws/async/wsfvt/server/DocLitWrappedPortImpl.java, WAS.websvcs.fvt, WSFP.WFVT 9/18/06 16:54:53 [9/26/06 09:46:44]
-//
-// IBM Confidential OCO Source Material
-// (C) COPYRIGHT International Business Machines Corp. 2006
-// The source code for this program is not published or otherwise divested
-// of its trade secrets, irrespective of what has been deposited with the
-// U.S. Copyright Office.
-//
-// Change History:
-// Date     UserId      Defect          Description
-// ----------------------------------------------------------------------------
-// 09/18/06 sedov       LIDB3296.38     New File
-// 09/26/06 sedov       393143          Added targetNS
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.apache.axis2.jaxws.sample.asyncdoclit.server;
 
