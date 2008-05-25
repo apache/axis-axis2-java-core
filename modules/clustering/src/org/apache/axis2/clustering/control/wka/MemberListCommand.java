4ad0902b2af1bfb098afc1c06cae3f69174019ec
Mohamed Afkham Azeez <azeez@apache.org>
2008-05-25 10:08:05 +0000

    Log message

--- modules/clustering/src/org/apache/axis2/clustering/control/wka/MemberListCommand.java
+++ modules/clustering/src/org/apache/axis2/clustering/control/wka/MemberListCommand.java
@@ -55,6 +55,7 @@ public class MemberListCommand extends ControlCommand {
     public void execute(ConfigurationContext configurationContext) throws ClusteringFault {
         log.info("Received MEMBER_LIST message");
         for (Member member : members) {
+            log.info("Trying to add member " + TribesUtil.getHost(member) + "...");
             Member localMember = membershipManager.getLocalMember();
             if (!(Arrays.equals(localMember.getHost(), member.getHost()) &&
                   localMember.getPort() == member.getPort())) {
