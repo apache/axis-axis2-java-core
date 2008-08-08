aaf6fef87169b0dd98a231cedae140acfea30a68
Brian DePradine <pradine@apache.org>
2008-08-08 08:31:49 +0000

    Fix unit test failure.

--- modules/addressing/test/org/apache/axis2/handlers/addressing/AddressingValidationHandlerTest.java
+++ modules/addressing/test/org/apache/axis2/handlers/addressing/AddressingValidationHandlerTest.java
@@ -78,18 +78,6 @@ public class AddressingValidationHandlerTest extends TestCase implements Address
         }
     }
 
-// TODO - commenting this out for the moment to get the build working.  This should either go away or get fixed!
-//    public void testValidateActionFlag() throws Exception {
-//        MessageContext mc = testAddressingMessage("valid-messages", "soapmessage.xml");
-//
-//        // Tell validation handler NOT to check action dispatch
-//        mc.setProperty(AddressingConstants.ADDR_VALIDATE_ACTION, Boolean.FALSE);
-//
-//        // Even though this message has an action that will not dispatch to an
-//        // AxisOperation, this shouldn't throw a fault.
-//        validationHandler.invoke(mc);
-//    }
-
     public void testMessageWithOmittedMessageIDInOnlyMEP() throws Exception {
         MessageContext messageContext = testMessageWithOmittedHeaders("noMessageID");
         String messageID = messageContext.getOptions().getMessageId();
