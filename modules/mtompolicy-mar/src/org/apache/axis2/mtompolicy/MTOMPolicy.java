91769a09b3f4c4ea98d656c4bc4be8ff63981adf
Nandana Mihindukulasooriya <nandana@apache.org>
2008-11-05 09:28:48 +0000

    MTOMPolicy module improvements

--- modules/mtompolicy-mar/src/org/apache/axis2/mtompolicy/MTOMPolicy.java
+++ modules/mtompolicy-mar/src/org/apache/axis2/mtompolicy/MTOMPolicy.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.axis2.mtompolicy;
 
 import org.apache.axis2.AxisFault;
@@ -51,18 +70,19 @@ public class MTOMPolicy implements Module {
         }
 
         AxisService axisService = Utils.locateAxisService(axisDescription);
-        
+
         if (axisService == null) {
             if (log.isDebugEnabled()) {
                 log.debug("MTOMPolicy module couldn't find the Axis Service ");
             }
             return;
         }
-        
-        Parameter param = axisService.getParameter(Constants.Configuration.ENABLE_MTOM);
-        
+
+        Parameter param = axisService
+                .getParameter(Constants.Configuration.ENABLE_MTOM);
+
         Policy mtomPolicy = Utils.getMTOMPolicy(param);
-        
+
         if (mtomPolicy != null) {
             Utils.applyPolicyToSOAPBindings(axisService, mtomPolicy);
         }
