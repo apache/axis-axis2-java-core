ca3f2de003e1e68851e29572dd5253137f30b0a8
Andreas Veithen <veithen@apache.org>
2009-02-27 15:08:57 +0000

    Added some Javadoc.

--- modules/kernel/src/org/apache/axis2/builder/Builder.java
+++ modules/kernel/src/org/apache/axis2/builder/Builder.java
@@ -25,10 +25,30 @@ import org.apache.axis2.context.MessageContext;
 
 import java.io.InputStream;
 
+/**
+ * Message builder able to convert a byte stream into a SOAP infoset.
+ * Message builders are used by {@link org.apache.axis2.transport.TransportListener}
+ * implementations to process the raw payload of the message and turn it into SOAP.
+ * Transports should use
+ * {@link org.apache.axis2.builder.BuilderUtil#getBuilderFromSelector(String, MessageContext)}
+ * to select the message builder appropriate for the content type of the message.
+ */
 public interface Builder {
 
     /**
-     * @return Returns the document element.
+     * Process a message.
+     * <p>
+     * The raw content of the message is provided as an input stream. It is the responsibility
+     * of the caller (typically a transport implementation) to close the stream after the
+     * message has been processed (more precisely after the SOAP infoset returned by this method
+     * is no longer used). This implies that implementations are not required to consume the
+     * input stream during the execution of this method. This enables deferred parsing of the
+     * message.
+     * 
+     * @param inputStream the byte stream with the raw payload
+     * @param contentType
+     * @param messageContext
+     * @return The SOAP infoset for the given message.
      */
     public OMElement processDocument(InputStream inputStream, String contentType,
                                      MessageContext messageContext) throws AxisFault;
