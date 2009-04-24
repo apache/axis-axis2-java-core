fea124aa73faa74d4e19bb00481bbc4fff1cf45c
Dennis M. Sosnoski <dsosnoski@apache.org>
2009-04-24 11:07:18 +0000

    Fix Axis2-4303 problem with quickstartjibx sample

--- modules/jibx/src/org/apache/axis2/jibx/NullBindingFactory.java
+++ modules/jibx/src/org/apache/axis2/jibx/NullBindingFactory.java
@@ -28,9 +28,11 @@ import org.jibx.runtime.impl.BindingFactoryBase;
  */
 public class NullBindingFactory extends BindingFactoryBase implements IBindingFactory {
 
+    private static final String[] EMPTY_ARRAY = new String[0];
+    
     public NullBindingFactory() {
-        super("", 0, 0, null, null, null, null, null, null, null, null, null, null, null, null,
-            null, null, null);
+        super("null", 0, 0, "", "", "", "", EMPTY_ARRAY, EMPTY_ARRAY, "", "",
+            EMPTY_ARRAY, "", "", "", "", "", EMPTY_ARRAY);
     }
 
     public String getCompilerDistribution() {
