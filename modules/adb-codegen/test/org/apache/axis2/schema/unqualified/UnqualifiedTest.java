18057a7c1c5b884bb244c00adba564b62fea81b5
Andreas Veithen <veithen@apache.org>
2009-06-25 20:00:55 +0000

    Added regression test for AXIS2-4374.

--- modules/adb-codegen/test/org/apache/axis2/schema/unqualified/UnqualifiedTest.java
+++ modules/adb-codegen/test/org/apache/axis2/schema/unqualified/UnqualifiedTest.java
@@ -20,12 +20,45 @@
 package org.apache.axis2.schema.unqualified;
 
 import org.apache.axis2.schema.AbstractTestCase;
+import org.apache.axis2.schema.unqualified.companyflow.CompanyResponse;
+import org.apache.axis2.schema.unqualified.companyservice.GetCompanyDetailsResponse;
+import org.apache.axis2.schema.unqualified.companytype.Address_type;
+import org.apache.axis2.schema.unqualified.companytype.String_Type;
 
 public class UnqualifiedTest extends AbstractTestCase {
-    public void test() throws Exception {
+    public void testTestElement() throws Exception {
         TestElement testElement = new TestElement();
         testElement.setElement1("element1");
         testElement.setElement2("element2");
         testSerializeDeserialize(testElement);
+    }
+    
+    /**
+     * Regression test for AXIS2-4374.
+     * 
+     * @throws Exception
+     */
+    public void testGetCompanyDetailsResponse() throws Exception {
+        String_Type companyId = new String_Type();
+        companyId.setString("112233");
+        
+        String_Type userId = new String_Type();
+        userId.setString("user");
+        
+        String_Type address1 = new String_Type();
+        address1.setString("Fenchurch street");
+        
+        Address_type address = new Address_type();
+        address.setAddress1(address1);
+        
+        CompanyResponse company = new CompanyResponse();
+        company.setCompanyId(companyId);
+        company.setUserId(userId);
+        company.setAddress(address);
+        
+        GetCompanyDetailsResponse response = new GetCompanyDetailsResponse();
+        response.setOut(company);
+        
+        testSerializeDeserialize(response);
     }
 }
