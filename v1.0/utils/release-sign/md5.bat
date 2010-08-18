7d3a7b2f074bd98623ddc3bc46d6adda18dee4c5
Andreas Veithen <veithen@apache.org>
2010-08-18 19:15:08 +0000

    INFRA-2441: Removing the extra 'java' component from the tag path.

--- v1.0/utils/release-sign/md5.bat
+++
@@ -1,5 +0,0 @@
-rem generates the MD5 hash of a file using the openssl binary
-rem argument - filename to be md5 calculated
-openssl md5 < %1 > %1.md5
-gpg --armor --output %1.asc --detach-sig %1
-gpg --verify %1.asc %1
