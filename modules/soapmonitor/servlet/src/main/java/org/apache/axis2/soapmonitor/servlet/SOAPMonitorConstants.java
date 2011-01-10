d220b941f923a609b2e9a14c33577d276aebef95
Mohamed Afkham Azeez <azeez@apache.org>
2011-01-10 16:28:28 +0000

    Fix for AXIS2-4500. Suggested by Bhushan Khardekar.
    Also did some code alignment since the 1 tab = 4 spaces rule was violated

--- modules/soapmonitor/servlet/src/main/java/org/apache/axis2/soapmonitor/servlet/SOAPMonitorConstants.java
+++ modules/soapmonitor/servlet/src/main/java/org/apache/axis2/soapmonitor/servlet/SOAPMonitorConstants.java
@@ -25,19 +25,24 @@ package org.apache.axis2.soapmonitor.servlet;
 
 public class SOAPMonitorConstants {
 
-  /**
-   * SOAP message types
-   */
-  public static final int SOAP_MONITOR_REQUEST  = 0;
-  public static final int SOAP_MONITOR_RESPONSE = 1;
+    /**
+     * SOAP message types
+     */
+    public static final int SOAP_MONITOR_REQUEST = 0;
+    public static final int SOAP_MONITOR_RESPONSE = 1;
 
-  /** 
-   * Servlet initialization parameter names
-   */
-  public static final String SOAP_MONITOR_PORT = "SOAPMonitorPort";
+    /**
+     * Port Servlet initialization parameter
+     */
+    public static final String SOAP_MONITOR_PORT = "SOAPMonitorPort";
 
-  /**
-   * Unique SOAP monitor id tag
-   */
-  public static final String SOAP_MONITOR_ID = "SOAPMonitorId";
+    /**
+     * Host name Servlet initialization parameter
+     */
+    public static final String SOAP_MONITOR_HOST_NAME = "SOAPMonitorHostName";
+
+    /**
+     * Unique SOAP monitor id tag
+     */
+    public static final String SOAP_MONITOR_ID = "SOAPMonitorId";
 }
