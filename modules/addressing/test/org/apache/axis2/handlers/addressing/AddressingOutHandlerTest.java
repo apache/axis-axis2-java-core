2f7c997c252553b03f1f3d28e8ed71264b0d91a0
Andreas Veithen <veithen@apache.org>
2011-04-05 21:13:08 +0000

    Removed unused imports.

--- modules/addressing/test/org/apache/axis2/handlers/addressing/AddressingOutHandlerTest.java
+++ modules/addressing/test/org/apache/axis2/handlers/addressing/AddressingOutHandlerTest.java
@@ -19,13 +19,11 @@
 
 package org.apache.axis2.handlers.addressing;
 
-import junit.framework.TestCase;
 import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMAttribute;
 import org.apache.axiom.om.OMNamespace;
 import org.apache.axiom.om.OMXMLParserWrapper;
 import org.apache.axiom.om.impl.OMNamespaceImpl;
-import org.apache.axiom.om.impl.llom.util.XMLComparator;
 import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axiom.soap.SOAPFactory;
 import org.apache.axiom.soap.SOAPHeaderBlock;
@@ -47,7 +45,6 @@ import javax.xml.namespace.QName;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 public class AddressingOutHandlerTest extends XMLTestCase implements AddressingConstants {
     private AddressingOutHandler outHandler;
