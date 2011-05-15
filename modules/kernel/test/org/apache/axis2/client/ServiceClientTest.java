884d65692afca99708e8928b9509d12f268f85f6
Andreas Veithen <veithen@apache.org>
2011-05-15 09:41:45 +0000

    AXIS2-4353: XmlSchema 1.4.8-SNAPSHOT now correctly resolves imports relative to JAR style URLs. Finalized and enabled the test case.

--- modules/kernel/test/org/apache/axis2/client/ServiceClientTest.java
+++ modules/kernel/test/org/apache/axis2/client/ServiceClientTest.java
@@ -20,15 +20,17 @@ package org.apache.axis2.client;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
 
 import javax.xml.namespace.QName;
 
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.ConfigurationContextFactory;
-import org.junit.Ignore;
+import org.apache.ws.commons.schema.XmlSchema;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class ServiceClientTest {
+public class ServiceClientTest extends Assert {
     /**
      * Tests that imported schemas are correctly resolved if the WSDL is loaded from a ZIP file.
      * This is a regression test for AXIS2-4353 and checks that WSDLs (with imports) can be loaded
@@ -36,7 +38,7 @@ public class ServiceClientTest {
      * 
      * @throws Exception
      */
-    @Test @Ignore
+    @Test
     public void testWSDLWithImportsFromZIP() throws Exception {
         ConfigurationContext configContext = ConfigurationContextFactory.createEmptyConfigurationContext();
         String basedir = System.getProperty("basedir");
@@ -45,6 +47,10 @@ public class ServiceClientTest {
         }
         URL zipUrl = new File(basedir, "target/test-zip.zip").toURL();
         URL wsdlUrl = new URL("jar:" + zipUrl + "!/test.wsdl");
-        new ServiceClient(configContext, wsdlUrl, new QName("urn:test", "EchoService"), "EchoPort");
+        ServiceClient serviceClient = new ServiceClient(configContext, wsdlUrl, new QName("urn:test", "EchoService"), "EchoPort");
+        List<XmlSchema> schemas = serviceClient.getAxisService().getSchema();
+        assertEquals(1, schemas.size());
+        XmlSchema schema = schemas.get(0);
+        assertNotNull(schema.getTypeByName(new QName("urn:test", "echoResponse")));
     }
 }
