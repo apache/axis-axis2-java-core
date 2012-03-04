6de603584232ef75241f7f9b9b4f9635719e6cb8
Andreas Veithen <veithen@apache.org>
2012-03-04 11:29:09 +0000

    Merged r1296062 to the 1.5 branch.

--- modules/saaj/src/org/apache/axis2/saaj/SOAPEnvelopeImpl.java
+++ modules/saaj/src/org/apache/axis2/saaj/SOAPEnvelopeImpl.java
@@ -20,6 +20,7 @@
 package org.apache.axis2.saaj;
 
 import org.apache.axiom.om.OMNode;
+import org.apache.axiom.om.impl.dom.ChildNode;
 import org.apache.axiom.om.impl.dom.DocumentImpl;
 import org.apache.axiom.om.impl.dom.NodeImpl;
 import org.apache.axiom.om.impl.dom.TextImpl;
@@ -201,7 +202,7 @@ public class SOAPEnvelopeImpl extends SOAPElementImpl implements javax.xml.soap.
             doomText.setNextOMSibling((OMNode)firstChild);
             doomText.setPreviousOMSibling(null);
             element.setFirstChild(doomText);
-            ((NodeImpl)firstChild).setPreviousOMSibling(doomText);
+            ((ChildNode)firstChild).setPreviousOMSibling(doomText);
         }
         return this;
     }
