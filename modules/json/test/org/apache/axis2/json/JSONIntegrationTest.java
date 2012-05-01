950a0d03100911b5a9fbeda31f3a2a82333cd5c1
Andreas Veithen <veithen@apache.org>
2012-05-01 21:18:45 +0000

    Added an integration test for Badgerfish with a POJO service.

--- modules/json/test/org/apache/axis2/json/JSONIntegrationTest.java
+++ modules/json/test/org/apache/axis2/json/JSONIntegrationTest.java
@@ -19,7 +19,9 @@
 
 package org.apache.axis2.json;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMFactory;
@@ -34,10 +36,20 @@ import org.apache.axis2.description.AxisService;
 import org.apache.axis2.testutils.PortAllocator;
 import org.apache.axis2.transport.http.SimpleHTTPServer;
 import org.apache.axis2.util.Utils;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class JSONIntegrationTest extends TestCase implements JSONTestConstants {
+public class JSONIntegrationTest implements JSONTestConstants {
 
     private static AxisService service;
 
@@ -47,44 +59,38 @@ public class JSONIntegrationTest extends TestCase implements JSONTestConstants {
 
     private static ConfigurationContext configurationContext;
 
-    private static EndpointReference targetEPR;
+    private static EndpointReference echoTargetEPR;
+    private static String pojoUri;
 
-    public JSONIntegrationTest() {
-    }
+    @BeforeClass
+    public static void startServer() throws Exception {
+        int testingPort = PortAllocator.allocatePort();
+        echoTargetEPR = new EndpointReference(
+                "http://127.0.0.1:" + (testingPort)
+                        + "/axis2/services/EchoXMLService/echoOM");
+        pojoUri = "http://127.0.0.1:" + testingPort + "/axis2/services/POJOService";
 
-    private static int count = 0;
+        File configFile =
+                new File(System.getProperty("basedir", ".") + "/test-resources/axis2.xml");
+        configurationContext = ConfigurationContextFactory.createConfigurationContextFromFileSystem(
+                System.getProperty("basedir", ".") + "/test-repository",
+                configFile.getAbsolutePath());
+        server = new SimpleHTTPServer(configurationContext, testingPort);
+        try {
+            server.start();
+        } finally {
 
-    protected void setUp() throws Exception {
-        if (count == 0) {
-            int testingPort = PortAllocator.allocatePort();
-            targetEPR = new EndpointReference(
-                    "http://127.0.0.1:" + (testingPort)
-                            + "/axis2/services/EchoXMLService/echoOM");
-
-            File configFile =
-                    new File(System.getProperty("basedir", ".") + "/test-resources/axis2.xml");
-            configurationContext = ConfigurationContextFactory
-                    .createConfigurationContextFromFileSystem(null, configFile
-                            .getAbsolutePath());
-            server = new SimpleHTTPServer(configurationContext, testingPort);
-            try {
-                server.start();
-            } finally {
-
-            }
-            service = Utils.createSimpleService(serviceName,
-                                                org.apache.axis2.json.Echo.class.getName(),
-                                                operationName);
-            server.getConfigurationContext().getAxisConfiguration().addService(
-                    service);
         }
-        count++;
+        service = Utils.createSimpleService(serviceName,
+                                            org.apache.axis2.json.Echo.class.getName(),
+                                            operationName);
+        server.getConfigurationContext().getAxisConfiguration().addService(
+                service);
     }
 
-    protected void tearDown() throws Exception {
-    	if(count == 3){
-    		server.stop();
-    	}
+    @AfterClass
+    public static void stopServer() throws Exception {
+		server.stop();
     }
 
     protected OMElement createEnvelope() throws Exception {
@@ -105,7 +111,7 @@ public class JSONIntegrationTest extends TestCase implements JSONTestConstants {
     private void doEchoOM(String messageType, String httpMethod) throws Exception{
     	OMElement payload = createEnvelope();
         Options options = new Options();
-        options.setTo(targetEPR);
+        options.setTo(echoTargetEPR);
         options.setProperty(Constants.Configuration.MESSAGE_TYPE, messageType);
         options.setTransportInProtocol(Constants.TRANSPORT_HTTP);
         options.setProperty(Constants.Configuration.HTTP_METHOD, httpMethod);
@@ -113,26 +119,43 @@ public class JSONIntegrationTest extends TestCase implements JSONTestConstants {
         ServiceClient sender = new ServiceClient(configurationContext, null);
         options.setAction(null);
         sender.setOptions(options);
-        options.setTo(targetEPR);
+        options.setTo(echoTargetEPR);
         OMElement result = sender.sendReceive(payload);
         OMElement ele = (OMElement)result.getFirstOMChild();
         compareWithCreatedOMText(ele.getText());
     }
 
-
+    @Test
     public void testEchoOMWithJSONBadgerfish() throws Exception{
     	doEchoOM("application/json/badgerfish", Constants.Configuration.HTTP_METHOD_POST);
     }
 
+    @Test
     public void testEchoOMWithJSON() throws Exception {
     	doEchoOM("application/json", Constants.Configuration.HTTP_METHOD_POST);
     }
 
+    @Test
     public void testEchoOMWithJSONInGET() throws Exception {
         doEchoOM("application/json", Constants.Configuration.HTTP_METHOD_GET);
     }
 
+    @Test
+    public void testPOJOServiceWithJSONBadgerfish() throws Exception {
+        HttpURLConnection conn = (HttpURLConnection)new URL(pojoUri).openConnection();
+        conn.setDoOutput(true);
+        conn.addRequestProperty("Content-Type", "application/json/badgerfish");
+        Writer out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+        // XML is: <sayHello xmlns="http://example.org"><myName>Joe</myName></sayHello>
+        out.write("{ \"sayHello\" : { \"@xmlns\" : { \"$\" : \"http://example.org\" }, \"myName\" : { \"$\" : \"Joe\" } } }");
+        out.close();
+        assertEquals(200, conn.getResponseCode());
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+        assertTrue(in.readLine().contains("Hello Joe!"));
+        in.close();
+    }
+    
     protected void compareWithCreatedOMText(String response) {
-        TestCase.assertEquals(response, expectedString);
+        assertEquals(response, expectedString);
     }
 }
