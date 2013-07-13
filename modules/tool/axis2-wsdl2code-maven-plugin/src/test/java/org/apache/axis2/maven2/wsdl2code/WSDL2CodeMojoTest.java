b864fe3e52f6eb179d7764dc0e9ef8cbc0511572
Sagara Gunathunga <sagara@apache.org>
2013-07-13 05:39:50 +0000

    Applied patch for AXIS2-5496

--- modules/tool/axis2-wsdl2code-maven-plugin/src/test/java/org/apache/axis2/maven2/wsdl2code/WSDL2CodeMojoTest.java
+++ modules/tool/axis2-wsdl2code-maven-plugin/src/test/java/org/apache/axis2/maven2/wsdl2code/WSDL2CodeMojoTest.java
@@ -29,10 +29,16 @@ import java.util.HashSet;
 public class WSDL2CodeMojoTest extends AbstractMojoTestCase {
     /** Tests running the java generator. */
     public void testJava() throws Exception {
-        runTest("src/test/test1", "wsdl2code");
+        runTest("src/test/test1", "wsdl2code", "src/main/axis2/service.wsdl");
     }
 
-    protected WSDL2CodeMojo newMojo(String pDir, String pGoal) throws Exception {
+    /** This test is added to test wsdl2codegen when there is schema import
+     * involved and the wsdl path contains space character */
+    public void testSchemaImport() throws Exception {
+        runTest("src/test/test2", "wsdl2code", "src/main/axis2/test dir/service.wsdl");
+    }
+
+    protected WSDL2CodeMojo newMojo(String pDir, String pGoal, String baseFilePath) throws Exception {
         File baseDir = new File(new File(getBasedir()), pDir);
         File testPom = new File(baseDir, "pom.xml");
         WSDL2CodeMojo mojo = (WSDL2CodeMojo)lookupMojo(pGoal, testPom);
@@ -40,7 +46,7 @@ public class WSDL2CodeMojoTest extends AbstractMojoTestCase {
         project.setDependencyArtifacts(new HashSet());
         setVariableValueToObject(mojo, "project", project);
         setVariableValueToObject(mojo, "wsdlFile",
-                                 new File(baseDir, "src/main/axis2/service.wsdl").getAbsolutePath());
+                                 new File(baseDir, baseFilePath).getAbsolutePath());
         setVariableValueToObject(mojo, "outputDirectory",
                                  new File(baseDir, "target/generated-sources/axis2/wsdl2code"));
         setVariableValueToObject(mojo, "syncMode", "both");
@@ -51,8 +57,8 @@ public class WSDL2CodeMojoTest extends AbstractMojoTestCase {
         return mojo;
     }
 
-    protected void runTest(String pDir, String pGoal)
+    protected void runTest(String pDir, String pGoal, String baseFilePath)
             throws Exception {
-        newMojo(pDir, pGoal).execute();
+        newMojo(pDir, pGoal, baseFilePath).execute();
     }
 }
