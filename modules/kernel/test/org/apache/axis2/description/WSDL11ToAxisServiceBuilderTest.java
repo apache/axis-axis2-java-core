d4946b481d6819748d0ad9781d602e73018d2bb4
Andreas Veithen <veithen@apache.org>
2016-02-14 17:12:36 +0000

    AXIS2-5732: Merge r1730335 to the 1.7 branch.

--- modules/kernel/test/org/apache/axis2/description/WSDL11ToAxisServiceBuilderTest.java
+++ modules/kernel/test/org/apache/axis2/description/WSDL11ToAxisServiceBuilderTest.java
@@ -23,11 +23,16 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.wsdl.xml.WSDLLocator;
 import javax.xml.namespace.QName;
 
 import org.apache.axis2.engine.AxisConfiguration;
+import org.apache.axis2.wsdl.WSDLConstants;
+import org.xml.sax.InputSource;
 
 import junit.framework.TestCase;
 
@@ -111,6 +116,172 @@ public class WSDL11ToAxisServiceBuilderTest extends TestCase {
 	        String expectedFaultAction = String.format("%s/VersionPortType/Fault/%sException", prefix, operationQName.getLocalPart());
 	        
 	        checkOperationActions(service, operation, expectedInputAction,expectedOutputAction,expectedFaultAction);
+        }
+    }
+    
+    /**
+     * Tests parsing of <code>EchoService.wsdl</code> into an AxisService instance. The <code>EchoService.wsdl</code>
+     * imports <code>EchoBindings.wsdl</code>, which contains the binding definitions (note that it does not reverse-import the EchoService.wsdl).
+     * Parsing the wsdl should cause neither a "There is no port type associated with the binding" exception, nor a "Cannot determine the MEP" exception,
+     * provided that Axis2 does not recursively search the port type in all imported wsdls, but looks it up via {@link javax.wsdl.Definition#getPortType(QName)}, 
+     * falling back to {@link javax.wsdl.Binding#getPortType()} if not available (will be the case when port type is imported through another document but not directly)
+     *
+     * The test verifies that no exceptions are thrown and additionally checks that the endpoint, binding, operation and its in/out messages are correctly populated from the WSDL definition.
+     * @throws IOException
+     */
+    public void testImportedBindings() throws IOException {
+        AxisConfiguration axisConf = new AxisConfiguration();
+        WSDLLocator wsdlLocator = new CustomWSDLLocator("test-resources/wsdl/imports/binding/", "EchoService.wsdl");
+        AxisService service = populateAxisService(axisConf, new File(wsdlLocator.getBaseURI()), wsdlLocator);
+        assertValidEchoService(service, wsdlLocator);
+    }
+    
+    /**
+     * Tests parsing of <code>EchoService.wsdl</code> into an AxisService instance. The <code>EchoService.wsdl</code>
+     * imports <code>EchoBindings.wsdl</code>, which reverse-imports the EchoService.wsdl (i.e. we have a circular import).
+     * Parsing the wsdl should cause neither a "There is no port type associated with the binding" exception, nor a "Cannot determine the MEP" exception,
+     * provided that Axis2 does not recursively search the port type in all imported wsdls, but looks it up via {@link javax.wsdl.Definition#getPortType(QName)}, 
+     * falling back to {@link javax.wsdl.Binding#getPortType()} if not available (will be the case when port type is imported through another document but not directly)
+     *
+     * The test verifies that no exceptions are thrown and additionally checks that the endpoint, binding, operation and its in/out messages are correctly populated from the WSDL definition.
+     * @throws IOException
+     */
+    public void testCircularImportedBindings() throws IOException {
+        AxisConfiguration axisConf = new AxisConfiguration();
+        WSDLLocator wsdlLocator = new CustomWSDLLocator("test-resources/wsdl/imports/binding_recursive/", "EchoService.wsdl");
+        AxisService service = populateAxisService(axisConf, new File(wsdlLocator.getBaseURI()), wsdlLocator);
+        assertValidEchoService(service, wsdlLocator);
+    }
+    
+    /**
+     * Tests parsing of <code>EchoService.wsdl</code> into an AxisService instance. The <code>EchoService.wsdl</code>
+     * imports <code>EchoBindings.wsdl</code>, which in turn imports the <code>EchoPortType.wsdl</code>.
+     * Parsing the wsdl should cause neither a "There is no port type associated with the binding" exception, nor a "Cannot determine the MEP" exception,
+     * provided that Axis2 does not recursively search the port type in all imported wsdls, but looks it up via {@link javax.wsdl.Definition#getPortType(QName)}, 
+     * falling back to {@link javax.wsdl.Binding#getPortType()} if not available (will be the case when port type is imported through another document but not directly)
+     *
+     * The test verifies that no exceptions are thrown and additionally checks that the endpoint, binding, operation and its in/out messages are correctly populated from the WSDL definition.
+     * @throws IOException
+     */
+    public void testImportedPortType() throws IOException {
+        AxisConfiguration axisConf = new AxisConfiguration();
+        WSDLLocator wsdlLocator = new CustomWSDLLocator("test-resources/wsdl/imports/portType/", "EchoService.wsdl");
+        AxisService service = populateAxisService(axisConf, new File(wsdlLocator.getBaseURI()), wsdlLocator);
+        assertValidEchoService(service, wsdlLocator);
+    }
+    
+    private void assertValidEchoService(AxisService echoService, WSDLLocator wsdlLocator) {
+        //check soap12 endpoint and binding are available
+        String endpointName = "EchoServiceHttpSoap12Endpoint";
+        AxisEndpoint soap12Endpoint = echoService.getEndpoint(endpointName);
+        assertNotNull(String.format("Cannot find %s endpoint in wsdl definition: %s", endpointName, wsdlLocator.getBaseURI()), soap12Endpoint);
+        
+        AxisBinding soap12Binding = soap12Endpoint.getBinding();
+        assertNotNull(String.format("Binding not set on %s endpoint in wsdl definition: %s", endpointName, wsdlLocator.getBaseURI()), soap12Binding);
+        
+        //check that policy reference is present on the binding subject
+        String bindingPolicyRefId = "#basicAuthPolicy";
+        assertNotNull(String.format("Cannot find policy reference %s on binding %s", bindingPolicyRefId, soap12Binding.getName()),
+                soap12Binding.getPolicySubject().getAttachedPolicyComponent(bindingPolicyRefId));
+        
+        //check that binding operation and respective operation are available
+        QName echoBindingOpName = new QName("http://tempuri.org/bindings", "echo");
+        AxisBindingOperation echoBindingOp = (AxisBindingOperation) soap12Binding.getChild(echoBindingOpName);
+        assertNotNull(String.format("Cannot find %s binding operation on binding %s",  echoBindingOpName, soap12Binding), echoBindingOp);
+        
+        AxisOperation echoOp = echoBindingOp.getAxisOperation();
+        assertNotNull(String.format("Operation not set on binding operation %s",  echoBindingOp.getName()), echoOp);
+        
+        //check that operation style is correctly identified and an operation with respective mep is created
+        assertTrue(String.format("Operation %s is not an instance of %s", echoOp.getName(), InOutAxisOperation.class.getName()), echoOp instanceof InOutAxisOperation);
+        assertEquals(String.format("Operation %s specifies an unexpected MEP uri: %s", echoOp.getName(), echoOp.getMessageExchangePattern()), 
+                WSDL2Constants.MEP_URI_IN_OUT, echoOp.getMessageExchangePattern());
+        
+        //check in/out messages
+        AxisMessage inMessage =  echoOp.getMessage(WSDLConstants.MESSAGE_LABEL_IN_VALUE);
+        assertNotNull(String.format("No input message set on operation %s", echoOp.getName()), inMessage);
+        QName echoInMessageName = new QName("http://tempuri.org/types", "echo");
+        assertEquals(String.format("Unexpected input message QName set on operation %s", echoOp.getName()), echoInMessageName, inMessage.getElementQName());
+        //message name is 'echoRequest' whereas the QName's local name is simply 'echo'
+        assertEquals(String.format("Unexpected input message name set on operation %s", echoOp.getName()), "echoRequest", inMessage.getName());
+        
+        AxisMessage outMessage =  echoOp.getMessage(WSDLConstants.MESSAGE_LABEL_OUT_VALUE);
+        assertNotNull(String.format("No output message set on operation %s", echoOp.getName()), outMessage);
+        QName echoOutMessageName = new QName("http://tempuri.org/types", "echoResponse");
+        assertEquals(String.format("Unexpected output message QName set on operation %s", echoOp.getName()), echoOutMessageName, outMessage.getElementQName());
+        assertEquals(String.format("Unexpected output message name set on operation %s", echoOp.getName()), echoOutMessageName.getLocalPart(), outMessage.getName());
+    }
+    
+    private AxisService populateAxisService(AxisConfiguration axisConf, File wsdlFile, WSDLLocator wsdlLocator) throws IOException {
+        InputStream in = null;
+        try {
+            in = new FileInputStream(wsdlFile);
+            WSDL11ToAxisServiceBuilder wsdl11Builder = new WSDL11ToAxisServiceBuilder(in);
+            if (wsdlLocator != null) {
+                wsdl11Builder.setCustomWSDLResolver(wsdlLocator);
+            }
+            wsdl11Builder.setDocumentBaseUri(wsdlFile.getParentFile().toURI().toString());
+            if (axisConf != null) {
+                wsdl11Builder.useAxisConfiguration(axisConf);
+            }
+            AxisService service = wsdl11Builder.populateService();
+            assertNotNull("Could not load AxisService from wsdl: " + wsdlFile.getAbsolutePath(), service);
+            
+            return service;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+    
+    //custom locator that can located imported wsdls relative to the base wsdl uri (code borrowed from wsdl4j)
+    class CustomWSDLLocator implements WSDLLocator {
+        private String wsdlURI;
+
+        private InputSource baseInputSource;
+        private Map<String, InputSource> importInputSources = new HashMap<String, InputSource>();
+        private boolean closed = false;
+        private String baseURI;
+        private String lastImportURI;
+
+        public CustomWSDLLocator(String baseURI, String wsdlURI) {
+            this.baseURI = baseURI;
+            this.wsdlURI = wsdlURI;
+        }
+
+        public InputSource getBaseInputSource() {
+            if (baseInputSource == null) {
+                lastImportURI = baseURI + wsdlURI;
+                baseInputSource = new InputSource(lastImportURI);
+            }
+            return baseInputSource;
+        }
+
+        public InputSource getImportInputSource(String parentLocation, String importLocation) {
+            InputSource inSource = (InputSource) importInputSources.get(importLocation);
+            if (inSource == null) {
+                lastImportURI = baseURI + importLocation;
+                inSource = new InputSource(lastImportURI);
+                importInputSources.put(importLocation, inSource);
+            }
+            return inSource;
+        }
+
+        public String getBaseURI() {
+            return baseURI + wsdlURI;
+        }
+
+        public String getLatestImportURI() {
+            return lastImportURI;
+        }
+
+        public void close() {
+            closed = true;
+        }
+
+        public boolean isClosed() {
+            return closed;
         }
     }
 }
