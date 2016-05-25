9ff67ccc4d3121b4837d717c580e9ee1f7a43cf0
Andreas Veithen <veithen@apache.org>
2016-05-25 06:20:20 +0000

    AXIS2-4791: Reimplement the invalid character filter as an MTOMXMLStreamWriter wrapper. This is possible since MTOMXMLStreamWriter is now an abstract class.

--- modules/jaxws/src/org/apache/axis2/datasource/jaxb/JAXBDSContext.java
+++ modules/jaxws/src/org/apache/axis2/datasource/jaxb/JAXBDSContext.java
@@ -21,7 +21,6 @@ package org.apache.axis2.datasource.jaxb;
 
 import org.apache.axiom.om.OMException;
 import org.apache.axiom.om.impl.MTOMXMLStreamWriter;
-import org.apache.axiom.om.util.XMLStreamWriterRemoveIllegalChars;
 import org.apache.axiom.util.stax.XMLStreamReaderUtils;
 import org.apache.axiom.util.stax.xop.XOPEncodedStream;
 import org.apache.axiom.util.stax.xop.XOPUtils;
@@ -360,90 +359,83 @@ public class JAXBDSContext {
         if (log.isDebugEnabled()) {
             log.debug("enter marshal");
         }
-        boolean installedFilter = false;
 
-        try {
-            // There may be a preferred classloader that should be used
-            ClassLoader cl = getClassLoader();
+        // There may be a preferred classloader that should be used
+        ClassLoader cl = getClassLoader();
 
 
-            // Very easy, use the Context to get the Marshaller.
-            // Use the marshaller to write the object.
-            JAXBContext jbc = getJAXBContext(cl);
-            Marshaller m = JAXBUtils.getJAXBMarshaller(jbc);
-            if (writer instanceof MTOMXMLStreamWriter && ((MTOMXMLStreamWriter) writer).getOutputFormat() != null) {
-                String encoding = ((MTOMXMLStreamWriter) writer).getOutputFormat().getCharSetEncoding();
+        // Very easy, use the Context to get the Marshaller.
+        // Use the marshaller to write the object.
+        JAXBContext jbc = getJAXBContext(cl);
+        Marshaller m = JAXBUtils.getJAXBMarshaller(jbc);
+        if (writer instanceof MTOMXMLStreamWriter && ((MTOMXMLStreamWriter) writer).getOutputFormat() != null) {
+            String encoding = ((MTOMXMLStreamWriter) writer).getOutputFormat().getCharSetEncoding();
 
-                String marshallerEncoding = (String) m.getProperty(Marshaller.JAXB_ENCODING);
+            String marshallerEncoding = (String) m.getProperty(Marshaller.JAXB_ENCODING);
 
-                // Make sure that the marshaller respects the encoding of the message.
-                // This is accomplished by setting the encoding on the Marshaller's JAXB_ENCODING property.
-                if (encoding == null && marshallerEncoding == null) {
-                    if (log.isDebugEnabled()) {
-                        log.debug("The encoding and the marshaller's JAXB_ENCODING are both set to the default (UTF-8)");
-                    }
-                } else {
-                    // Must set the encoding to an actual String to set it on the Marshaller
-                    if (encoding == null) {
-                        encoding = "UTF-8";
-                    }
-                    if (!encoding.equalsIgnoreCase(marshallerEncoding)) {
-                        if (log.isDebugEnabled()) {
-                            log.debug("The Marshaller.JAXB_ENCODING is " + marshallerEncoding);
-                            log.debug("The Marshaller.JAXB_ENCODING is changed to the message encoding " + 
-                                    encoding);
-                        }
-                        m.setProperty(Marshaller.JAXB_ENCODING, encoding);
-                    } else {
-                        if (log.isDebugEnabled()) {
-                            log.debug("The encoding and the marshaller's JAXB_ENCODING are both set to:" + 
-                                    marshallerEncoding);
-                        }
-                    }
+            // Make sure that the marshaller respects the encoding of the message.
+            // This is accomplished by setting the encoding on the Marshaller's JAXB_ENCODING property.
+            if (encoding == null && marshallerEncoding == null) {
+                if (log.isDebugEnabled()) {
+                    log.debug("The encoding and the marshaller's JAXB_ENCODING are both set to the default (UTF-8)");
                 }
-            }
-
-            AttachmentMarshaller am = new JAXBAttachmentMarshaller(createAttachmentContext(), writer);
-            if (am != null) {
-                if (DEBUG_ENABLED) {
-                    log.debug("Adding JAXBAttachmentMarshaller to Marshaller");
-                }
-                m.setAttachmentMarshaller(am);
-            }
-
-            MessageContext mc = getMessageContext();
-
-            // If requested install a filter to remove illegal characters
-            installedFilter = installFilter(mc, writer);
-
-
-            // Marshal the object
-            if (getProcessType() == null) {
-                marshalByElement(obj, 
-                        m, 
-                        writer, 
-                        true);
-                //!am.isXOPPackage());
             } else {
-                marshalByType(obj,
-                        m,
-                        writer,
-                        getProcessType(),
-                        isxmlList(),
-                        getConstructionType(),
-                        true); // Attempt to optimize by writing to OutputStream
+                // Must set the encoding to an actual String to set it on the Marshaller
+                if (encoding == null) {
+                    encoding = "UTF-8";
+                }
+                if (!encoding.equalsIgnoreCase(marshallerEncoding)) {
+                    if (log.isDebugEnabled()) {
+                        log.debug("The Marshaller.JAXB_ENCODING is " + marshallerEncoding);
+                        log.debug("The Marshaller.JAXB_ENCODING is changed to the message encoding " + 
+                                encoding);
+                    }
+                    m.setProperty(Marshaller.JAXB_ENCODING, encoding);
+                } else {
+                    if (log.isDebugEnabled()) {
+                        log.debug("The encoding and the marshaller's JAXB_ENCODING are both set to:" + 
+                                marshallerEncoding);
+                    }
+                }
             }
+        }
 
-            JAXBUtils.releaseJAXBMarshaller(jbc, m);
+        AttachmentMarshaller am = new JAXBAttachmentMarshaller(createAttachmentContext(), writer);
+        if (am != null) {
+            if (DEBUG_ENABLED) {
+                log.debug("Adding JAXBAttachmentMarshaller to Marshaller");
+            }
+            m.setAttachmentMarshaller(am);
+        }
 
-            if (log.isDebugEnabled()) {
-                log.debug("exit marshal");
-            }
-        } finally {
-            // Make sure the filter is uninstalled
-            if (installedFilter) {
-                uninstallFilter(writer);
-            }
+        MessageContext mc = getMessageContext();
+
+        // If requested install a filter to remove illegal characters
+        if (writer instanceof MTOMXMLStreamWriter && ContextUtils.isJAXBRemoveIllegalChars(mc)) {
+            writer = new XMLStreamWriterRemoveIllegalChars((MTOMXMLStreamWriter)writer);
+        }
+
+        // Marshal the object
+        if (getProcessType() == null) {
+            marshalByElement(obj, 
+                    m, 
+                    writer, 
+                    true);
+            //!am.isXOPPackage());
+        } else {
+            marshalByType(obj,
+                    m,
+                    writer,
+                    getProcessType(),
+                    isxmlList(),
+                    getConstructionType(),
+                    true); // Attempt to optimize by writing to OutputStream
+        }
+
+        JAXBUtils.releaseJAXBMarshaller(jbc, m);
+
+        if (log.isDebugEnabled()) {
+            log.debug("exit marshal");
         }
     }
     
@@ -1093,37 +1085,4 @@ public class JAXBDSContext {
             throw new OMException(t);
         }
     }
-    
-
-    /**
-     * Install a JAXB filter if requested
-     * @param mc
-     * @param writer
-     * @return true if filter installed
-     */
-    private boolean installFilter(MessageContext mc, XMLStreamWriter writer) {
-        if (!(writer instanceof MTOMXMLStreamWriter)) {
-            return false;
-        }
-        if (!ContextUtils.isJAXBRemoveIllegalChars(mc)) {
-            return false;
-        }
-        
-         
-        MTOMXMLStreamWriter mtomWriter = (MTOMXMLStreamWriter) writer;
-        mtomWriter.setFilter(new XMLStreamWriterRemoveIllegalChars());
-        return true;
-    }
-    
-    /**
-     * UninstallInstall a JAXB filter if requested
-     * @param mc
-     * @param writer
-     * @return true if filter installed
-     */
-    private void uninstallFilter(XMLStreamWriter writer) {
-        MTOMXMLStreamWriter mtomWriter = (MTOMXMLStreamWriter) writer;
-        mtomWriter.removeFilter();
-    }
-
 }
