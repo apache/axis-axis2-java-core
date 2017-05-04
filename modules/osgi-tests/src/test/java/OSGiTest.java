981b0871f1a1694890d1d52bdb7be95e29b5688a
Andreas Veithen <veithen@apache.org>
2017-05-04 20:16:20 +0000

    AXIS2-4310: Merge r1793723 to the 1.7 branch.

--- modules/osgi-tests/src/test/java/OSGiTest.java
+++ modules/osgi-tests/src/test/java/OSGiTest.java
@@ -61,7 +61,6 @@ public class OSGiTest {
                 url("link:classpath:org.apache.felix.configadmin.link"),
                 url("link:classpath:org.apache.servicemix.bundles.wsdl4j.link"),
                 url("link:classpath:org.apache.geronimo.specs.geronimo-activation_1.1_spec.link"), // TODO: should not be necessary on Java 6
-                url("link:classpath:org.apache.geronimo.specs.geronimo-jms_1.1_spec.link"), // TODO: why the heck is this required???
                 url("link:classpath:org.apache.geronimo.specs.geronimo-ws-metadata_2.0_spec.link"),
                 url("link:classpath:org.apache.geronimo.specs.geronimo-javamail_1.4_spec.link"), // TODO: should no longer be necessary
                 url("link:classpath:org.apache.geronimo.specs.geronimo-servlet_2.5_spec.link"),
