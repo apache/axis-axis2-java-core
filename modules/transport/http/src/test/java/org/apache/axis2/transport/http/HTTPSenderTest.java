2f8f783c616b9afb2dcb843a654a3bf5ab53a292
mcmics <MCMicS@users.noreply.github.com>
2025-03-17 20:39:08 +0100

    add Test for cookie Handling

--- modules/transport/http/src/test/java/org/apache/axis2/transport/http/HTTPSenderTest.java
+++ modules/transport/http/src/test/java/org/apache/axis2/transport/http/HTTPSenderTest.java
@@ -20,10 +20,8 @@
 package org.apache.axis2.transport.http;
 
 import org.apache.axis2.Constants;
-import org.apache.axis2.context.ConfigurationContext;
-import org.apache.axis2.context.ConfigurationContextFactory;
-import org.apache.axis2.context.MessageContext;
-import org.apache.axis2.context.OperationContext;
+import org.apache.axis2.context.*;
+import org.apache.axis2.kernel.http.HTTPConstants;
 import org.apache.axis2.transport.http.mock.server.AbstractHTTPServerTest;
 import org.apache.axis2.transport.http.mock.server.BasicHttpServer;
 
@@ -58,13 +56,16 @@ public abstract class HTTPSenderTest extends AbstractHTTPServerTest {
      * @throws IOException
      *             Signals that an I/O exception has occurred.
      */
-    protected void sendViaHTTP(String httpMethod, String soapAction, String address, boolean rest)
+    protected MessageContext sendViaHTTP(String httpMethod, String soapAction, String address, boolean rest)
             throws IOException {
         httpSender = getHTTPSender();
+        ServiceContext serviceContext = new ServiceContext();
         MessageContext msgContext = new MessageContext();
+        msgContext.setServiceContext(serviceContext);
         ConfigurationContext configContext = ConfigurationContextFactory
                 .createEmptyConfigurationContext();
         OperationContext opContext = new OperationContext();
+        opContext.setParent(serviceContext);
 
         msgContext.setConfigurationContext(configContext);
         msgContext.setEnvelope(getEnvelope());
@@ -73,7 +74,7 @@ public abstract class HTTPSenderTest extends AbstractHTTPServerTest {
         msgContext.setOperationContext(opContext);
         URL url = new URL(address);
         httpSender.send(msgContext, url, soapAction);
-
+        return msgContext;
     }
 
     /**
@@ -302,6 +303,17 @@ public abstract class HTTPSenderTest extends AbstractHTTPServerTest {
         getBasicHttpServer().setResponseTemplate(BasicHttpServer.RESPONSE_HTTP_500);
         sendViaHTTP(Constants.Configuration.HTTP_METHOD_POST, "urn:postService",
                 "http://localhost:" + port + "/postService", true);
+    }
+
+    public void testCookiesAreObtainedAfterRequest() throws Exception {
+        httpSender = getHTTPSender();
+        int port = getBasicHttpServer().getPort();
+        getBasicHttpServer().setResponseTemplate(BasicHttpServer.RESPONSE_HTTP_COOKIE);
+        final MessageContext mc = sendViaHTTP(Constants.Configuration.HTTP_METHOD_POST, "urn:postService",
+                "http://localhost:" + port + "/postService", true);
+
+        assertEquals("Cookie was not set", "JSESSIONID=abcde12345",
+                mc.getProperty(HTTPConstants.COOKIE_STRING));
     }
     
 
